70b0b6f6a9022bfd473786aab359a2ff1c046210
Desmond O. Chang <dochang@gmail.com>
2015-08-06 08:28:42 +0800

    bugfix

--- shell/profile.sh
+++ shell/profile.sh
@@ -143,7 +143,7 @@ prepend_to_env ${LINUXBREW_ROOT}/lib LD_LIBRARY_PATH
 
 ## nix
 # Expand HOME for Nix installer check.
-if [ -z "${NIX_LINK}" -a -e "{{ HOME }}/.nix-profile/etc/profile.d/nix.sh" ]; then
+if [ -z "${NIX_PATH}" -a -e "{{ HOME }}/.nix-profile/etc/profile.d/nix.sh" ]; then
 	. "{{ HOME }}/.nix-profile/etc/profile.d/nix.sh"
 fi
 
