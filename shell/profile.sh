1e61f225d8499ce73fc2ada53b6c703f3be8fa31
Desmond O. Chang <dochang@gmail.com>
2015-08-17 18:53:03 +0800

    Reset GPG_TTY every time a login shell starts.

--- shell/profile.sh
+++ shell/profile.sh
@@ -22,7 +22,7 @@ export SSH_ASKPASS
 : ${SUDO_ASKPASS:=/usr/bin/ssh-askpass}
 export SUDO_ASKPASS
 ## sudo requires an absolute path to program for SUDO_ASKPASS.
-: ${GPG_TTY:=$(tty)}
+GPG_TTY=$(tty)
 export GPG_TTY
 
 
