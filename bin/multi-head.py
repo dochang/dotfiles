b1dbc4c9e8ad33e7084c687f152cd549623b1756
Desmond O. Chang <dochang@gmail.com>
2016-01-21 16:49:16 +0800

    xrandr2json

--- bin/multi-head.py
+++ bin/multi-head.py
@@ -1,62 +1,9 @@
 #!/usr/bin/env python
 
-# This code is borrowed from: https://github.com/substack/xrandr-parse
-
 from subprocess import check_output
 import subprocess
-import re
 from functools import cmp_to_key
-
-regex = {
-    'connected': re.compile('^(\S+) connected (?:(\d+)x(\d+))?'),
-    'disconnected': re.compile('^(\S+) disconnected'),
-    'mode': re.compile('^\s+(\d+)x(\d+)\s+((?:\d+\.)?\d+)([* ]?)([+ ]?)'),
-}
-
-def parse(src):
-    lines = src.splitlines()
-    query = {}
-    last = None
-    index = 0
-
-    for line in lines:
-        m = regex['connected'].search(line)
-        if m:
-            index += 1
-            query[m.group(1)] = {
-                'connected': True,
-                'modes': [],
-                'index': index,
-            }
-            if (m.group(2) and m.group(3)):
-                query[m.group(1)]['width'] = int(m.group(2))
-                query[m.group(1)]['height'] = int(m.group(3))
-            last = m.group(1)
-            continue
-        m = regex['disconnected'].search(line)
-        if m:
-            index += 1
-            query[m.group(1)] = {
-                'connected': False,
-                'modes': [],
-                'index': index,
-            }
-            last = m.group(1)
-            continue
-        m = regex['mode'].search(line)
-        if last and m:
-            r = {
-                'width': int(m.group(1)),
-                'height': int(m.group(2)),
-                'rate': float(m.group(3)),
-            }
-            query[last]['modes'].append(r)
-            if (m.group(4) == '*'): query[last]['current'] = r
-            if (m.group(5) == '+'): query[last]['preferred'] = r
-            continue
-        last = None
-
-    return query
+import json
 
 def get_config(query):
     config = {}
@@ -91,8 +38,7 @@ def get_config(query):
         y += height
     return config
 
-def multi_head_setup(query):
-    config = get_config(query)
+def multi_head_setup(config):
     cmd = ['xrandr']
     for k in config.keys():
         v = config[k]
@@ -106,8 +52,10 @@ def multi_head_setup(query):
     subprocess.call(cmd)
 
 def main():
-    output = check_output(['xrandr'], universal_newlines=True)
-    multi_head_setup(parse(output))
+    output = check_output(['xrandr2json'], universal_newlines=True)
+    query = json.loads(output)
+    config = get_config(query)
+    multi_head_setup(config)
 
 if __name__ == '__main__':
     main()
