6c850bb36a255a7d53acdc0e84a8d709dc90964f
Desmond O. Chang <dochang@gmail.com>
2016-03-21 01:08:44 +0800

    Move `ANDROID_HOME`

--- shell/profile.sh
+++ shell/profile.sh
@@ -140,7 +140,7 @@ esac
 export CHROME_DEVEL_SANDBOX
 
 ## For Android SDK
-: ${ANDROID_HOME:=/opt/android-sdk-linux}
+: ${ANDROID_HOME:=/opt/android-sdk}
 export ANDROID_HOME
 prepend_to_env ${ANDROID_HOME}/tools PATH
 prepend_to_env ${ANDROID_HOME}/platform-tools PATH
