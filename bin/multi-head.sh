2ebb3e6c01d88ea1c2f031e675bbc814d09995bf
Desmond O. Chang <dochang@gmail.com>
2016-04-20 03:05:32 +0800

    multi-head

--- bin/multi-head.sh
+++ bin/multi-head.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-xrandr2json | multi-head.py | json2xrandr
+CONF_DIR="${XDG_CONFIG_DIR:-${HOME}/.config}/multi-head"
+CONF_FILE="${CONF_DIR}/multi-head.py"
+
+if [ -x "${CONF_FILE}" ] ; then
+	xrandr2json | "${CONF_FILE}" | json2xrandr
+fi
