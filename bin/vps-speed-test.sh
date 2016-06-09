d2757e14044fd6d6ac421ce4c54350e553b06d90
Desmond O. Chang <dochang@gmail.com>
2016-06-10 02:04:33 +0800

    Allow to customize variables

--- bin/vps-speed-test.sh
+++ bin/vps-speed-test.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 # https://www.linode.com/speedtest
-LINODE_TEST_TYPE=100MB
-LINODE_REGIONS='newark atlanta dallas fremont frankfurt london singapore tokyo'
+: "${LINODE_TEST_TYPE:=100MB}"
+: "${LINODE_REGIONS:=newark atlanta dallas fremont frankfurt london singapore tokyo}"
+[ "${LINODE_REGIONS}" = - ] && LINODE_REGIONS=
 
 # http://speedtest-${region}.digitalocean.com/
-DO_TEST_TYPE=100mb
-DO_REGIONS='nyc1 nyc2 nyc3 ams1 ams2 ams3 sfo1 sgp1 lon1 fra1 tor1 blr1'
+: "${DO_TEST_TYPE:=100mb}"
+: "${DO_REGIONS:=nyc1 nyc2 nyc3 ams1 ams2 ams3 sfo1 sgp1 lon1 fra1 tor1 blr1}"
+[ "${DO_REGIONS}" = - ] && DO_REGIONS=
 
 # https://www.vultr.com/faq/#downloadspeedtests
-VULTR_TEST_TYPE=1000MB
-VULTR_REGIONS='fra-de ams-nl par-fr lon-gb hnd-jp nj-us il-us ga-us wa-us fl-us tx-us sjo-ca-us lax-ca-us syd-au'
+: "${VULTR_TEST_TYPE:=1000MB}"
+: "${VULTR_REGIONS:=fra-de ams-nl par-fr lon-gb hnd-jp nj-us il-us ga-us wa-us fl-us tx-us sjo-ca-us lax-ca-us syd-au}"
+[ "${VULTR_REGIONS}" = - ] && VULTR_REGIONS=
 
 unset http_proxy
 unset https_proxy
