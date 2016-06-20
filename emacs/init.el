ad049da37cfa69d97c509b927ad0fd5e39c30ef2
Desmond O. Chang <dochang@gmail.com>
2016-06-20 22:35:48 +0800

    Split Emacs config: compile

--- emacs/init.el
+++ emacs/init.el
@@ -1907,22 +1907,6 @@ from 'todotxt-file'." t)
 (global-set-key [XF86Mail] 'gnus)
 
 
-;;; Compilation
-
-(defun $eval-after-load-compile ()
-  ;; Compilation for Gradle
-  ;;
-  ;; - [[http://kjeldahl.net/d7/node/34]]
-  ;; - [[https://github.com/mariusk/android-with-emacs/blob/fb65f49666766e8c25b23b0377d086f6e55a3f5b/README.md]]
-  (unless (assoc 'gradle compilation-error-regexp-alist-alist)
-    (setq compilation-error-regexp-alist-alist
-          (cons (list 'gradle ":compile.*?\\(/.*?\\):\\([0-9]+\\): " 1 2)
-                compilation-error-regexp-alist-alist)))
-  (add-to-list 'compilation-error-regexp-alist 'gradle))
-
-(eval-after-load 'compile '($eval-after-load-compile))
-
-
 (load (locate-user-emacs-file "bootstrap"))
 
 
