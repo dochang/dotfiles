3b86c467409983bc7dd7932909d1f67f4218af30
Desmond O. Chang <dochang@gmail.com>
2016-06-21 04:22:31 +0800

    Split Emacs config: nxml

---
+++ emacs/init.d/nxml.el
@@ -0,0 +1,17 @@
+;;; nXML Mode
+
+(defun $nxml-mode-hook ()
+  ($prog-mode-hook*)
+  ($camel-case-mode 1)
+  (setq indent-tabs-mode nil))
+
+(req-package nxml
+  :loader :built-in
+  :init
+  (add-hook 'nxml-mode-hook '$nxml-mode-hook)
+  :config
+  ;; Make nXML indentation variables safe as file local variables if
+  ;; their values satisfy the predicate `integerp'.
+  (put 'nxml-child-indent 'safe-local-variable 'integerp)
+  (put 'nxml-outline-child-indent 'safe-local-variable 'integerp)
+  (put 'nxml-attribute-indent 'safe-local-variable 'integerp))
--- emacs/init.el
+++ emacs/init.el
@@ -1484,24 +1484,6 @@ major mode isn't derived from `prog-mode'."
 (add-hook 'sgml-mode-hook '$sgml-mode-hook)
 
 
-;;; nXML Mode
-(defun $nxml-mode-hook ()
-  ($prog-mode-hook*)
-  ($camel-case-mode 1)
-  (setq indent-tabs-mode nil))
-
-(add-hook 'nxml-mode-hook '$nxml-mode-hook)
-
-(defun $eval-after-load-nxml-mode ()
-  ;; Make nXML indentation variables safe as file local variables if
-  ;; their values satisfy the predicate `integerp'.
-  (put 'nxml-child-indent 'safe-local-variable 'integerp)
-  (put 'nxml-outline-child-indent 'safe-local-variable 'integerp)
-  (put 'nxml-attribute-indent 'safe-local-variable 'integerp))
-
-(eval-after-load 'nxml-mode '($eval-after-load-nxml-mode))
-
-
 (load (locate-user-emacs-file "bootstrap"))
 
 
