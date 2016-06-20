228421fc41a71e491e6041a772095ce44abd43ce
Desmond O. Chang <dochang@gmail.com>
2016-06-20 22:38:03 +0800

    Split Emacs config: gnus

--- emacs/init.el
+++ emacs/init.el
@@ -1889,24 +1889,6 @@ from 'todotxt-file'." t)
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
 
 
-;;; Gnus
-
-;; Inhibit the startup message.
-;;
-;; This variable should be set in `.emacs' instead of `.gnus'.
-;;
-;; [[info:gnus#Startup%20Variables]]
-(setq gnus-inhibit-startup-message t)
-
-;; Documentation is wrong.  Default will try "~/.authinfo" first.
-(setq auth-sources '("~/.authinfo.gpg" "~/.authinfo" "~/.netrc"))
-
-;; `shr' fails to display some HTML mails.  Use `gnus-w3m' instead.
-(setq mm-text-html-renderer 'gnus-w3m)
-
-(global-set-key [XF86Mail] 'gnus)
-
-
 (load (locate-user-emacs-file "bootstrap"))
 
 
