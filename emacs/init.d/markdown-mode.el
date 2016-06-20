dda412f37d9f445772239863e373000fb57f5e23
Desmond O. Chang <dochang@gmail.com>
2016-06-21 03:48:48 +0800

    Split Emacs config: markdown-mode

---
+++ emacs/init.d/markdown-mode.el
@@ -0,0 +1,27 @@
+;;; Markdown Mode
+;; [[http://jblevins.org/projects/markdown-mode/]]
+
+(defun $markdown-reset-auto-mode-alist ()
+  ;; Remove all `markdown-mode' related entries inserted by autoload code in
+  ;; `auto-mode-alist'.
+  (setq auto-mode-alist
+        (cons (cons "\\.\\(markdown\\|md\\|mdo?wn\\|mkdn?\\)\\'"
+                    'markdown-mode)
+              (rassq-delete-all 'markdown-mode auto-mode-alist))))
+
+(defun $markdown-mode-hook ()
+  ;; By default `markdown-mode' wants to handle `*.text' files (by autoloads),
+  ;; but I want to open them by `text-mode'.
+  ;;
+  ;; Because loading autoloads is the final step of package initialization, any
+  ;; configuration will be overriden.  This code has to be here rather than in
+  ;; `eval-after-load'.
+  (if ($file-name-match "\\.text\\'" buffer-file-name)
+      (progn
+        ($markdown-reset-auto-mode-alist)
+        (set-auto-mode t))
+    (setq indent-tabs-mode nil)))
+
+(req-package markdown-mode
+  :init
+  (add-hook 'markdown-mode-hook '$markdown-mode-hook))
--- emacs/init.el
+++ emacs/init.el
@@ -1646,31 +1646,6 @@ major mode isn't derived from `prog-mode'."
 (add-hook 'slim-mode-hook '$slim-mode-hook)
 
 
-;;; Markdown Mode
-;; [[http://jblevins.org/projects/markdown-mode/]]
-(defun $markdown-mode-hook ()
-  ;; By default `markdown-mode' wants to handle `*.text' files (by autoloads),
-  ;; but I want to open them by `text-mode'.
-  ;;
-  ;; Because loading autoloads is the final step of package initialization, any
-  ;; configuration will be overriden.  This code has to be here.
-  (if ($file-name-match "\\.text\\'" buffer-file-name)
-      (progn
-        ($markdown-reset-auto-mode-alist)
-        (set-auto-mode t))
-    (setq indent-tabs-mode nil)))
-
-(add-hook 'markdown-mode-hook '$markdown-mode-hook)
-
-(defun $markdown-reset-auto-mode-alist ()
-  ;; Remove all `markdown-mode' related entries inserted by autoload code in
-  ;; `auto-mode-alist'.
-  (setq auto-mode-alist
-        (cons (cons "\\.\\(markdown\\|md\\|mdo?wn\\|mkdn?\\)\\'"
-                    'markdown-mode)
-              (rassq-delete-all 'markdown-mode auto-mode-alist))))
-
-
 (load (locate-user-emacs-file "bootstrap"))
 
 
