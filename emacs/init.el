eb8edd7b4aaf265d63a7843a92d3e44a2995fa2e
Desmond O. Chang <dochang@gmail.com>
2016-06-20 16:14:15 +0800

    Split Emacs config: minimap

--- emacs/init.el
+++ emacs/init.el
@@ -1996,33 +1996,6 @@ from 'todotxt-file'." t)
 (setq whitespace-style '(face trailing tabs lines-tail newline))
 
 
-;;; Minimap
-;; [[http://www.emacswiki.org/emacs/MiniMap]]
-;;
-;; There're two versions currently:
-;;
-;;
-;; 1. David Engster <dengste@eml.cc>
-;;
-;; [[git://randomsample.de/minimap.git]]
-;; [[http://randomsample.de/minimap.git]]
-;; [[http://www.randomsample.de/minimap.el]]
-;;
-;; The original version.
-;;
-;; 2. Dustin Lacewell <dlacewell@gmail.com>
-;;
-;; [[http://ldlework.com/posts/2011-11-14-emacs-minimap.html]]
-;; [[https://github.com/dustinlacewell/emacs-minimap]]
-;;
-;; This version is based on Engster's with some improvements like only
-;; one single global minimap window.
-;;
-;;
-;; We use none of them since they neither display correctly.  Use sublimity
-;; instead.
-
-
 (load (locate-user-emacs-file "bootstrap"))
 
 
