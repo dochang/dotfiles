81fb94b9a9ca9667b3ac5da690164263e92b8f93
Desmond O. Chang <dochang@gmail.com>
2016-06-22 19:24:58 +0800

    Configure kotlin-mode

---
+++ emacs/init.d/init_kotlin-mode.el
@@ -0,0 +1,12 @@
+(defun $kotlin-mode-hook ()
+  ($prog-mode-hook*)
+  ($camel-case-mode 1)
+  (setq indent-tabs-mode nil))
+
+(req-package kotlin-mode
+  ;; For Kotlin script
+  ;;
+  ;; [[https://kotlinlang.org/docs/tutorials/command-line.html#using-the-command-line-to-run-scripts]]
+  :mode ("\\.kts\\'" . kotlin-mode)
+  :init
+  (add-hook 'kotlin-mode-hook '$kotlin-mode-hook))
