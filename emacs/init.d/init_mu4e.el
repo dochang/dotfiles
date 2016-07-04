71bc65f9b098653dc4c53f014741dd3a1fa8d8ed
Desmond O. Chang <dochang@gmail.com>
2016-07-04 17:54:07 +0800

    BCC the sender in mu4e automatically

--- emacs/init.d/init_mu4e.el
+++ emacs/init.d/init_mu4e.el
@@ -1,4 +1,10 @@
+(defun $mu4e-compose-mode-hook ()
+  (save-excursion
+    (let ((bcc-addr (message-field-value "From")))
+      (message-add-header (format "Bcc: %s" bcc-addr)))))
+
 (req-package mu4e
   :loader :path
   :init
-  (setq mu4e-attachment-dir (expand-file-name "~/Downloads/")))
+  (setq mu4e-attachment-dir (expand-file-name "~/Downloads/"))
+  (add-hook 'mu4e-compose-mode-hook '$mu4e-compose-mode-hook))
