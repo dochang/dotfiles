ad1cb69d417fd34c5a50ebba73b21ec994ed6335
Desmond O. Chang <dochang@gmail.com>
2016-07-12 12:37:20 +0800

    Force to unbind meta-tab in flyspell-mode

--- emacs/init.d/init_flyspell.el
+++ emacs/init.d/init_flyspell.el
@@ -3,4 +3,6 @@
 (req-package flyspell
   :loader :built-in
   :init
-  (setq flyspell-use-meta-tab nil))
+  (setq flyspell-use-meta-tab nil)
+  :config
+  (define-key flyspell-mode-map "\M-\t" nil))
