bf0b26a59224ecbd0b8264f997caa14471918d8e
Desmond O. Chang <dochang@gmail.com>
2016-12-17 15:58:17 +0800

    Reset enabled go linters

--- emacs/init.d/init_flycheck-gometalinter.el
+++ emacs/init.d/init_flycheck-gometalinter.el
@@ -1,4 +1,11 @@
 (req-package flycheck-gometalinter
   :init
   ;; skips 'vendor' directories and sets GO15VENDOREXPERIMENT=1
-  (setq flycheck-gometalinter-vendor t))
+  (setq flycheck-gometalinter-vendor t)
+  ;; Reset enabled linters.  See [1] for details.
+  ;;
+  ;; [1]: https://raw.githubusercontent.com/dasfoo/travis/master/go.sh
+  (setq flycheck-gometalinter-disable-linters
+        '("gas"))
+  (setq flycheck-gometalinter-enable-linters
+        '("testify" "test" "goimports" "gofmt" "lll" "misspell" "unused")))
