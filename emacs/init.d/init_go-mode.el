120db3749b56b36470d5ad7ef68874bce1734aa9
Desmond O. Chang <dochang@gmail.com>
2016-12-17 16:33:56 +0800

    Run both goimports & gofmt before saving

--- emacs/init.d/init_go-mode.el
+++ emacs/init.d/init_go-mode.el
@@ -6,10 +6,20 @@
   ($camel-case-mode 1)
   (setq indent-tabs-mode t))
 
+(defun $gofmt-before-save ()
+  (interactive)
+  (mapc (lambda (cmdline)
+          (let ((gofmt-command (car cmdline))
+                (gofmt-args (cdr cmdline)))
+            (when (executable-find gofmt-command)
+              (gofmt-before-save))))
+        '(("goimports")
+          ("gofmt" "-s"))))
+
 (req-package go-mode
   :init
   (setq gofmt-command
         (cond ((executable-find "goimports") "goimports")
               (t "gofmt")))
-  (add-hook 'before-save-hook 'gofmt-before-save)
+  (add-hook 'before-save-hook '$gofmt-before-save)
   (add-hook 'go-mode-hook '$go-mode-hook))
