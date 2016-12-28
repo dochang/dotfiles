fa1c512da438fa225bc1016c721029810bd075d7
Desmond O. Chang <dochang@gmail.com>
2016-12-29 00:08:24 +0800

    Install flycheck-rust

--- emacs/init.d/init_flycheck.el
+++ emacs/init.d/init_flycheck.el
@@ -1,4 +1,9 @@
+(defun $flycheck-mode-hook ()
+  (flycheck-rust-setup))
+
 (req-package flycheck
+  :init
+  (add-hook 'flycheck-mode-hook '$flycheck-mode-hook)
   :config
   (flycheck-package-setup)
   (flycheck-gometalinter-setup))
