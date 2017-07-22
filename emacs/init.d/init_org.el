1594db5ba69e45ef300d0b0b504191d44927dc80
Desmond O. Chang <dochang@gmail.com>
2017-07-22 18:48:11 +0800

    Configure org agenda behavior

--- emacs/init.d/init_org.el
+++ emacs/init.d/init_org.el
@@ -130,6 +130,27 @@
   (setq org-read-date-force-compatible-dates nil)
   ;; Show only one occurrence of a repeating timestamp in the agenda.
   (setq org-agenda-repeating-timestamp-show-all nil)
+  ;; Show deadline delay only after deadline in agenda.  Hide scheduled delay.
+  (setq org-agenda-skip-scheduled-delay-if-deadline 'post-deadline)
+  ;; Hide scheduled entry in agenda if the entry has been done.
+  (setq org-agenda-skip-scheduled-if-done t)
+  ;; Hide scheduled entry in agenda if deadline is shown and the scheduled is
+  ;; before the deadline.
+  (setq org-agenda-skip-scheduled-if-deadline-is-shown 'repeated-after-deadline)
+  ;; Hide timestamp entry in agenda if the entry has been done.
+  (setq org-agenda-skip-timestamp-if-done t)
+  ;; Hide timestamp entry in agenda if deadline is shown.
+  (setq org-agenda-skip-timestamp-if-deadline-is-shown t)
+  ;; Hide deadline entry in agenda if the entry has been done.
+  (setq org-agenda-skip-deadline-if-done t)
+  ;; Hide deadline prewarning if scheduled is shown and the prewarning is
+  ;; before the scheduled.
+  (setq org-agenda-skip-deadline-prewarning-if-scheduled 'pre-scheduled)
+  ;; Display all timestamps even if they are in the same day.  We have to do
+  ;; this.  Otherwise, the timestamps in the same day but with different times
+  ;; will be merged into the first one, and others will disappear in the
+  ;; agenda.
+  (setq org-agenda-skip-additional-timestamps-same-entry nil)
   ;; Track habits.
   (add-to-list 'org-modules 'org-habit)
   ;; Show habits in agenda buffers.
