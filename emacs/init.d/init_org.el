bd4c2c863fde63dbcab4f4764a2ae724dde2529e
Desmond O. Chang <dochang@gmail.com>
2017-07-28 20:23:42 +0800

    Edit `org-default-notes-file` in org-mode

--- emacs/init.d/init_org.el
+++ emacs/init.d/init_org.el
@@ -168,6 +168,8 @@
 (defvar **org-timer**)
 
 (req-package org-plus-contrib
+  :mode ("/\\.notes\\'" . org-mode)
+  ;; Edit `org-default-notes-file' in org-mode.
   :bind (("C-c l" . org-store-link)
          ("C-c a" . org-agenda)
          ("C-c b" . org-iswitchb)
