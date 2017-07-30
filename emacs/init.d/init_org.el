30402605e22466efcff0fbaa9a41a5d4f217d5f8
Desmond O. Chang <dochang@gmail.com>
2017-07-31 06:33:09 +0800

    Add timestamp function `$diary-chinese-date`

--- emacs/init.d/init_org.el
+++ emacs/init.d/init_org.el
@@ -1,5 +1,45 @@
 ;;; Org
 
+(defun $diary-chinese-date (month day &optional year mark)
+  "Specific date(s) diary entry.
+Entry applies if date is MONTH, DAY, YEAR.  Each parameter can be a
+list of integers, t (meaning all values), or an integer.  The order
+of the input parameters changes according to `calendar-date-style'
+\(e.g. to DAY MONTH YEAR in the European style).
+
+YEAR can also be nil, which means all values like t.
+
+An optional parameter MARK specifies a face or single-character string
+to use when highlighting the day in the calendar."
+  ;; Load `cal-china' for `calendar-chinese-from-absolute'.
+  (when (require 'cal-china nil t)
+    (pcase-let* ((ddate (diary-make-date month day year))
+                 (`(,dy ,dm ,dd)
+                  (list
+                   (calendar-extract-year ddate)
+                   (calendar-extract-month ddate)
+                   (calendar-extract-day ddate)))
+                 (`(,cc ,cy ,m ,d)      ; current chinese date
+                  (calendar-chinese-from-absolute
+                   (calendar-absolute-from-gregorian
+                    (or date (calendar-current-date)))))
+                 (y (+ (* cc 100) cy))
+                 ;; The chinese year is (+ (* cc 100) cy) actually.
+                 ;; See `calendar-chinese-from-absolute-for-diary' for details.
+                 )
+      (and
+       (or (and (listp dd) (memq d dd))
+           (equal d dd)
+           (eq dd t))
+       (or (and (listp dm) (memq m dm))
+           (equal m dm)
+           (eq dm t))
+       (or (not dy)
+           (and (listp dy) (memq y dy))
+           (equal y dy)
+           (eq dy t))
+       (cons mark entry)))))
+
 (defun $org-load-hook ()
   (setq org-directory (expand-file-name "~/org"))
   ;; Skip non-reachable files in `org-agenda-files'.
