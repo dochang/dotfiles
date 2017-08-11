b829c2afa43c15d2d14b9e6e9cc8af4706670cbd
Desmond O. Chang <dochang@gmail.com>
2017-08-11 16:45:41 +0800

    Fix loader of cal-china-plus

--- emacs/init.d/init_cal-china.el
+++ emacs/init.d/init_cal-china.el
@@ -2,5 +2,5 @@
     (req-package cal-china-plus
       :loader :el-get)
   ;; cal-china-plus has been merged into Emacs 25.1
-  (req-package cal-china-plus
-    :loader :built-in))
+  (req-package cal-china
+    :loader :path))
