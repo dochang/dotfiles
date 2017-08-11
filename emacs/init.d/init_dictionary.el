36dbe52ba59371d5dd6500525abced773f1a1717
Desmond O. Chang <dochang@gmail.com>
2017-08-11 13:40:52 +0800

    Use hydra for dictionary keybindings

--- emacs/init.d/init_dictionary.el
+++ emacs/init.d/init_dictionary.el
@@ -7,14 +7,18 @@
   (set (make-local-variable 'view-read-only) nil))
 
 (req-package dictionary
-  :bind (("C-c d s" . dictionary-search)
-         ("C-c d m" . dictionary-match-words)
-         ("C-c d d" . dictionary-lookup-definition)
+  :bind (("C-c d" . $hydra-dictionary/body)
          :map dictionary-mode-map
          ("DEL" . scroll-down))
   :init
   (setq dictionary-use-single-buffer t)
   (setq dictionary-tooltip-dictionary "!")
   (add-hook 'dictionary-mode-hook '$dictionary-mode-hook)
+  (defhydra $hydra-dictionary (:color teal)
+    "dictionary"
+    ("s" dictionary-search "search")
+    ("m" dictionary-match-words "match words")
+    ("d" dictionary-lookup-definition "lookup definition")
+    ("q" nil "quit"))
   :config
   (global-dictionary-tooltip-mode 1))
