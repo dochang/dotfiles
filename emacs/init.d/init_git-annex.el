57fea860580489a33bb4841c89b2832e89840048
Desmond O. Chang <dochang@gmail.com>
2017-08-11 17:13:52 +0800

    Fix git-annex config

--- emacs/init.d/init_git-annex.el
+++ emacs/init.d/init_git-annex.el
@@ -1,4 +1,6 @@
 (req-package git-annex
   ;; Avoid key binding conflicts.
-  :bind-keymap (:map $extended-map
-                ("@" . git-annex-dired-map)))
+  ;;
+  ;; `:bind-keymap' does not support `:map'.  Use `:init' instead.
+  :init
+  (define-key $extended-map "@" 'git-annex-dired-map))
