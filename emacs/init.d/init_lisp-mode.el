1f40c8b098c43d16c7662e3909f51cf6884f679a
Desmond O. Chang <dochang@gmail.com>
2017-08-11 12:25:08 +0800

    Split lisp-mode & elisp-mode config

--- emacs/init.d/init_lisp-mode.el
+++ emacs/init.d/init_lisp-mode.el
@@ -91,31 +91,6 @@ Lisp function does not specify a special indentation."
               (method
                (funcall method indent-point state))))))))
 
-(defun $init-file-p (file-name)
-  (setq file-name (expand-file-name ($buffer-file-name file-name)))
-  (cl-some (lambda (pattern) ($file-name-match pattern file-name))
-           (list "[]>:/\\]\\..*\\(emacs\\|gnus\\|viper\\)\\(\\.el\\)?\\'"
-                 "\\`\\..*emacs\\(\\.el\\)?\\'"
-                 "[:/]_emacs\\(\\.el\\)?\\'"
-                 "[]>:/\\]\\..*\\(emacs_local\\)\\(\\.el\\)?\\'"
-                 (rx buffer-start
-                     (eval (file-name-as-directory
-                            (expand-file-name user-emacs-directory)))))))
-
-(defun $emacs-lisp-mode-hook ()
-  ($prog-mode-hook*)
-  ($lisp-mode-common-hook)
-  (form-feed-mode 1)
-  ;; Flycheck is too strict for my `.emacs' now.  Use flymake instead.
-  (when (or (null buffer-file-name)
-            ($init-file-p buffer-file-name)
-            (not ($file-name-match "\\.el\\'" buffer-file-name)))
-    (flycheck-mode -1)
-    (flymake-mode 1))
-  ;; Eldoc Mode
-  ;; [[info:emacs#Lisp%20Doc]]
-  (eldoc-mode 1))
-
 (defun $lisp-mode-hook ()
   ($prog-mode-hook*)
   ($lisp-mode-common-hook))
@@ -123,27 +98,10 @@ Lisp function does not specify a special indentation."
 (req-package lisp-mode
   :loader :path
 
-  :mode ("Cask\\'" . emacs-lisp-mode)
-
   :init
-  ;; Paredit always inserts a space when I insert "(" after ",@".  Change the
-  ;; syntax descriptor of "@" from "_" to "'" will solve this problem.
-  ;;
-  ;; References:
-  ;;
-  ;;   - `paredit-space-for-delimiter-p'
-  ;;   - `emacs-lisp-mode-syntax-table'
-  (modify-syntax-entry ?@ "'   " emacs-lisp-mode-syntax-table)
-
-  (add-hook 'emacs-lisp-mode-hook '$emacs-lisp-mode-hook)
-
   (setq lisp-indent-function '$lisp-indent-function)
 
   ;; Use `fill-column' when filling docstrings.
   (setq emacs-lisp-docstring-fill-column t)
-
-  ;; Do not limit the output when evaluating.
-  (setq eval-expression-print-length nil)
-  (setq eval-expression-print-level nil)
 
   (add-hook 'lisp-mode-hook '$lisp-mode-hook))
