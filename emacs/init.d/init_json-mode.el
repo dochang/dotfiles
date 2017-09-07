f789778d27b455fab0f3a16cb7b6ccf4d152ef3e
Desmond O. Chang <dochang@gmail.com>
2017-09-07 11:27:38 +0800

    Force to disable prettier for `json-mode`
    Because `json-mode` is a derived mode of `js-mode`.

--- emacs/init.d/init_json-mode.el
+++ emacs/init.d/init_json-mode.el
@@ -1,7 +1,9 @@
 ;;; JSON Mode
 
 (defun $json-mode-hook ()
-  ($prog-mode-hook*))
+  ($prog-mode-hook*)
+  ;; prettier doesn't support JSON.  Disable it.
+  (prettier-js-mode -1))
 
 (req-package json-mode
   :mode (("\\.bowerrc\\'" . json-mode)
