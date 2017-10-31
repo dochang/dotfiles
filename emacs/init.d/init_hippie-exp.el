141364751700dba54397e0e9dd30c7cdc3ec6783
Desmond O. Chang <dochang@gmail.com>
2017-11-01 03:49:16 +0800

    Fix configuration of hippie-expand

--- emacs/init.d/init_hippie-exp.el
+++ emacs/init.d/init_hippie-exp.el
@@ -1,2 +1,13 @@
 (req-package hippie-exp
-  :bind (("M-/" . hippie-expand)))
+  :bind (("M-/" . hippie-expand))
+  :config
+  ;; Don't try to complete an entire line or list.  Those functions don't work
+  ;; well with paredit
+  ;;
+  ;; https://emacs.stackexchange.com/a/4249
+  ;; https://www.emacswiki.org/emacs/HippieExpand#toc9
+  (setq hippie-expand-try-functions-list
+        (cl-reduce (lambda (fn-list fn)
+                     (remove fn fn-list))
+                   '(try-expand-line try-expand-list)
+                   :initial-value hippie-expand-try-functions-list)))
