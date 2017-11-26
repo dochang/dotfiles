bb920c5f4e729b42c44cf62e481ed899460a6fc1
Desmond O. Chang <dochang@gmail.com>
2017-11-26 14:52:55 +0800

    Make ssh-agent a systemd user service

--- bin/X.sh
+++ bin/X.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-systemctl --user import-environment SSH_AGENT_PID SSH_AUTH_SOCK SSH_ASKPASS SUDO_ASKPASS
 multi-head.sh
 # Run multi head setting after encfs so that the askpass dialog will appear in
 # the center of the primary output.
