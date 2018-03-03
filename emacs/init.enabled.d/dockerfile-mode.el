dceb3a45f46731bb48c8e1e10d6d59567cc12585
Desmond O. Chang <dochang@gmail.com>
2018-03-03 20:03:33 +0800

    Disable `aggressive-indent-mode` in `dockerfile-mode`

--- emacs/init.enabled.d/dockerfile-mode.el
+++ emacs/init.enabled.d/dockerfile-mode.el
@@ -1,5 +1,12 @@
 ;;; Dockerfile Mode
 ;; [[https://github.com/spotify/dockerfile-mode]]
 
+(defun $dockerfile-mode-hook ()
+  ($prog-mode-hook*)
+  ;; aggressive-indent-mode will break the indentation.  Disable it.
+  (aggressive-indent-mode -1))
+
 (req-package dockerfile-mode
-  :mode ("Dockerfile\\'" . dockerfile-mode))
+  :mode ("Dockerfile\\'" . dockerfile-mode)
+  :init
+  (add-hook 'dockerfile-mode-hook '$dockerfile-mode-hook))
