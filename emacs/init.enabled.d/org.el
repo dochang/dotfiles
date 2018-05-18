37aa02d93207fc81f97802e1c1f7d15232f2ad02
Desmond O. Chang <dochang@gmail.com>
2018-05-18 13:10:33 +0800

    Enable exporting org to markdown

--- emacs/init.enabled.d/org.el
+++ emacs/init.enabled.d/org.el
@@ -165,7 +165,8 @@
   (setq org-habit-show-habits-only-for-today nil)
   ;; Do not show the consistency graph of the habits, which are not scheduled,
   ;; on today's agenda.
-  (setq org-habit-show-all-today nil))
+  (setq org-habit-show-all-today nil)
+  (add-to-list 'org-export-backends 'md))
 
 (defun $org-mode-hook ()
   (linum-mode 1)
