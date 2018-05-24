9362e00d31c23192df06484d1491b34d82d329e7
Desmond O. Chang <dochang@gmail.com>
2018-05-24 18:54:35 +0800

    Replace `to_unicode` with `to_text`
    https://github.com/ansible/ansible/commit/4ed88512e45112f9670560ac3f01707a40a7f5c5

--- filter_plugins/main.py
+++ filter_plugins/main.py
@@ -1,11 +1,11 @@
-from ansible.utils.unicode import to_unicode
+from ansible.module_utils._text import to_text
 
 import pytoml
 
 def to_toml(a, *args, **kw):
     '''Make verbose, human readable toml'''
     transformed = pytoml.dumps(a, **kw)
-    return to_unicode(transformed)
+    return to_text(transformed)
 
 class FilterModule(object):
     def filters(self):
