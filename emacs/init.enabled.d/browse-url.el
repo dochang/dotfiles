d5b9cda7ccbcd8e3fc7861942cb1f0daa979a627
Desmond O. Chang <dochang@gmail.com>
2018-08-06 21:24:44 +0800

    Remove some outdated functions

--- emacs/init.enabled.d/browse-url.el
+++ emacs/init.enabled.d/browse-url.el
@@ -1,34 +1,21 @@
 ;;; Browse URL
 
-(defun $browse-url-conkeror (url &optional new-window)
-  (interactive (browse-url-interactive-arg "URL: "))
-  (let ((browse-url-generic-program "conkeror")
-        (browse-url-generic-args nil))
-    (browse-url-generic url new-window)))
+(defmacro $browse-url-define-incognito-function (browser-name browser-args)
+  (declare (indent nil))
+  (let ((fname (intern (format "%s/incognito" browser-name))))
+    `(defun ,fname (url &optional new-window)
+       (interactive (browse-url-interactive-arg "URL: "))
+       (let ((,browser-args
+              (if (member "--incognito" ,browser-args)
+                  ,browser-args
+                (cons "--incognito" ,browser-args))))
+         (,browser-name url new-window)))))
 
-(defun $browse-url-chromium (url &optional new-window)
-  "Ask the Chromium WWW browser to load URL.
-Default to the URL around or before point.  The strings in
-variable `browse-url-chromium-arguments' are also passed to
-Chromium."
-  (interactive (browse-url-interactive-arg "URL: "))
-  (setq url (browse-url-encode-url url))
-  (let* ((process-environment (browse-url-process-environment)))
-    (apply 'start-process
-           (concat "chromium " url) nil
-           (or (bound-and-true-p browse-url-chromium-program) "chromium")
-           (append
-            (bound-and-true-p browse-url-chromium-arguments)
-            (list url)))))
+($browse-url-define-incognito-function browse-url-chromium
+                                       browse-url-chromium-arguments)
 
-(defun $browse-url-chromium/incognito (url &optional new-window)
-  (interactive (browse-url-interactive-arg "URL: "))
-  (let ((browse-url-chromium-arguments
-         (cons "--incognito" (bound-and-true-p browse-url-chromium-arguments))))
-    (cond ((<= emacs-major-version 23)
-           ($browse-url-chromium url new-window))
-          (t
-           (browse-url-chromium url new-window)))))
+($browse-url-define-incognito-function browse-url-chrome
+                                       browse-url-chrome-arguments)
 
 (defun $browse-url-sensible-browser (url &optional new-window)
   (interactive (browse-url-interactive-arg "URL: "))
