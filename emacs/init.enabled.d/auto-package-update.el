8992a2075eb6ea9ca8db6425b4c1b9efac75a0a0
Desmond O. Chang <dochang@gmail.com>
2018-08-20 19:38:29 +0800

    Fix advice of auto-package-update

--- emacs/init.enabled.d/auto-package-update.el
+++ emacs/init.enabled.d/auto-package-update.el
@@ -1,6 +1,10 @@
 ;;; auto-package-update
-(define-advice apu--packages-to-install (:filter-return (packages) delete-el-get-packages)
-  "auto-package-update MUST NOT update the packages installed by el-get."
-  (cl-set-difference packages el-get-activated-list))
+(define-advice apu--package-out-of-date-p (:around (fn &rest r) unavailable-packages-return-nil)
+  "Non package.el packages (e.g. el-get packages) and Nonupgradeable packages
+  are NOT up-to-date and NOT out-of-date."
+  (let ((pkg (first r)))
+    (and (package-installed-p pkg)
+         (cadr (assq pkg package-archive-contents))
+         (funcall fn pkg))))
 
 (req-package auto-package-update)
