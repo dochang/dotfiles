b748578c0878a2ef57ba082709ed97047a29cf04
Desmond O. Chang <dochang@gmail.com>
2018-08-23 01:59:16 +0800

    Do not modify file when installing packages via quelpa

--- emacs/init.el
+++ emacs/init.el
@@ -603,6 +603,24 @@ The call stack:
   ;; packages manually please.
   (setq quelpa-update-melpa-p nil))
 
+(define-advice quelpa-build--build-single-file-package (:around (fn &rest r) dont-update-time-stamp-and-copyright)
+  "This function runs `before-save-hook'.  Since installing package is a
+background operation, we must skip the hooks which modify the file and keep the
+file unmodified.
+
+The call stack:
+
+`quelpa'
+-> `quelpa-build--build-single-file-package'
+-> `write-file'
+-> `save-buffer'
+-> `basic-save-buffer'
+-> `before-save-hook'
+"
+  (let ((time-stamp-active nil)
+        (copyright-update nil))
+    (apply fn r)))
+
 
 (use-package quelpa-use-package
   :demand t
