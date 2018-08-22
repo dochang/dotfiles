43e6df073ce184fd2facdd53bfa02417e85e668e
Desmond O. Chang <dochang@gmail.com>
2018-08-23 04:32:34 +0800

    Install zenburn via quelpa

--- emacs/init.enabled.d/zenburn.el
+++ emacs/init.enabled.d/zenburn.el
@@ -1,6 +1,14 @@
+;; zenburn for color theme
+;;
+;; [[http://www.emacswiki.org/emacs/ColorThemeZenburn]]
+;;
+;; 1. [[https://github.com/dbrock/zenburn-el]]
+;; 2. [[https://github.com/bbatsov/zenburn-emacs/blob/0c46ca823dd007241c48778d38b80ac8bde6d5ee/color-theme-zenburn.el]]
+;;
+;; 1 supports more packages than 2.
+
 (req-package zenburn
   :ensure nil
-  :el-get t
+  :quelpa (zenburn :fetcher github :repo "dbrock/zenburn-el")
   :init
-  (autoload 'color-theme-zenburn "color-theme-zenburn" nil t)
   (add-to-list '**color-themes** 'color-theme-zenburn))
