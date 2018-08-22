76c681186e7e1278b4f9bd9793fa06eea27b5d08
Desmond O. Chang <dochang@gmail.com>
2018-08-23 04:34:18 +0800

    Add comments for zenburn-theme

--- emacs/init.enabled.d/zenburn-theme.el
+++ emacs/init.enabled.d/zenburn-theme.el
@@ -1,3 +1,15 @@
+;; zenburn for custom theme
+;;
+;; [[http://www.emacswiki.org/emacs/ColorThemeZenburn]]
+;;
+;; 1. [[https://github.com/bbatsov/zenburn-emacs]]
+;; 2. [[https://github.com/djcb/elisp/blob/master/themes/zenburn-theme.el]]
+;; 3. [[http://www.emacswiki.org/emacs/zenburn.el]]
+;;
+;; - 1-2 are for custom theme.
+;; - 2 is too old.
+;; - 3 is an old version of 1.
+
 (req-package zenburn-theme
   :init
   (add-to-list 'safe-local-eval-forms
