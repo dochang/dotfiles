75fc52164b4b5ad364b98921c3c2eeb866c4d291
Desmond O. Chang <dochang@gmail.com>
2018-08-23 01:15:27 +0800

    Explain why we have to install cflow via el-get

--- emacs/init.enabled.d/cflow.el
+++ emacs/init.enabled.d/cflow.el
@@ -1,7 +1,14 @@
 (req-package cflow
   :ensure nil
-  :quelpa (cflow
-           :fetcher git
-           :url "https://git.savannah.gnu.org/git/cflow.git"
-           :files ("elisp/cflow-mode.el"))
+  :el-get t
+  ;; The repo size of cflow is small, but the git repo has a module, gnulib,
+  ;; which is a big repo.  We can't `git clone` cflow via quelpa, because it
+  ;; always downloads git modules.  We should install cflow via quelpa's "url"
+  ;; fetcher, but we can't, due to this error:
+  ;;
+  ;; ```
+  ;; Error getting PACKAGE-DESC: (search-failed ;;; cflow.el ends here)
+  ;; ```
+  ;;
+  ;; Install cflow via el-get instead.
   :mode ("\\.cflow$" . cflow-mode))
