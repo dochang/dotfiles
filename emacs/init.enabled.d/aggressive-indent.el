c7d511a2dc3cba534eead26820a7d6e8bd745f06
Desmond O. Chang <dochang@gmail.com>
2018-08-22 22:52:08 +0800

    Disable `electric-indent-mode` when `aggressive-indent` enabled

--- emacs/init.enabled.d/aggressive-indent.el
+++ emacs/init.enabled.d/aggressive-indent.el
@@ -1,1 +1,11 @@
-(req-package aggressive-indent)
+(req-package aggressive-indent
+  :init
+  (setq aggressive-indent-dont-electric-modes t)
+  ;; `electric-indent-mode' should be disabled.  Otherwise the variable
+  ;; `electric-indent-mode' is t even `aggressive-indent-mode' is enabled.
+  ;;
+  ;; The variable `electric-indent-mode' controls
+  ;; `electric-newline-and-maybe-indent' (`C-j').  If `electric-indent-mode' is
+  ;; t, `electric-newline-and-maybe-indent' just inserts a newline, no
+  ;; indenting.
+  )
