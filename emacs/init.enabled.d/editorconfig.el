e2d74ee84499be065109da5a9e5435a45ac462a4
Desmond O. Chang <dochang@gmail.com>
2018-08-24 20:15:44 +0800

    Prettify code

--- emacs/init.enabled.d/editorconfig.el
+++ emacs/init.enabled.d/editorconfig.el
@@ -3,5 +3,7 @@
   (setq editorconfig-get-properties-function
         'editorconfig-core-get-properties-hash)
   :config
-  (add-to-list 'editorconfig-indentation-alist '(nxml-mode nxml-child-indent nxml-attribute-indent))
-  (add-to-list 'editorconfig-indentation-alist '(puppet-mode puppet-indent-level puppet-include-indent)))
+  (add-to-list 'editorconfig-indentation-alist
+               '(nxml-mode nxml-child-indent nxml-attribute-indent))
+  (add-to-list 'editorconfig-indentation-alist
+               '(puppet-mode puppet-indent-level puppet-include-indent)))
