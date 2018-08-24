2641922fa1c73fc2c4c4cbb2b02c4182e0340fce
Desmond O. Chang <dochang@gmail.com>
2018-08-24 21:55:19 +0800

    Reconfigure `slim-mode-hook`

--- emacs/init.enabled.d/slim-mode.el
+++ emacs/init.enabled.d/slim-mode.el
@@ -1,8 +1,4 @@
 ;;; Slim Mode
 ;; [[https://github.com/slim-template/emacs-slim]]
 
-(defun $slim-mode-hook ()
-  ($run-prog-mode-hook))
-
-(req-package slim-mode
-  :hook (slim-mode . $slim-mode-hook))
+(req-package slim-mode)
