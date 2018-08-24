1c781937588f1f6cc72a20736777b545bedf7a65
Desmond O. Chang <dochang@gmail.com>
2018-08-24 14:27:59 +0800

    Do not install executable as it's a built-in package

--- emacs/init.enabled.d/executable.el
+++ emacs/init.enabled.d/executable.el
@@ -1,4 +1,5 @@
 (req-package executable
+  :ensure nil
   :init
   ;; https://www.emacswiki.org/emacs/MakingScriptsExecutableOnSave
   (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p))
