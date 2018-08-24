d1ec0eda288a5e9e7cb9788601bb6f77b1aa9a7a
Desmond O. Chang <dochang@gmail.com>
2018-08-24 20:26:57 +0800

    It's safe to ensure `executable`
    It's a built-in package.

--- emacs/init.enabled.d/executable.el
+++ emacs/init.enabled.d/executable.el
@@ -1,5 +1,4 @@
 ;; https://www.emacswiki.org/emacs/MakingScriptsExecutableOnSave
 
 (req-package executable
-  :ensure nil
   :hook (after-save . executable-make-buffer-file-executable-if-script-p))
