7982eef3f6f79b16482401158686c24fb3e1c41f
Desmond O. Chang <dochang@gmail.com>
2018-08-24 21:52:59 +0800

    Reconfigure `scss-mode-hook`

--- emacs/init.enabled.d/scss-mode.el
+++ emacs/init.enabled.d/scss-mode.el
@@ -1,14 +1,7 @@
 ;;; Scss Mode
 ;; [[https://github.com/antonj/scss-mode]]
 
-(defun $scss-mode-hook ()
-  ;; No need to run `prog-mode-hook' if `scss-mode' is derived from `css-mode'.
-  (unless (derived-mode-p 'css-mode)
-    ($run-prog-mode-hook))
-  (rainbow-mode 1))
-
 (req-package scss-mode
-  :hook (scss-mode . $scss-mode-hook)
   :init
   ;; Don't compile after saving.
   (setq scss-compile-at-save nil))
