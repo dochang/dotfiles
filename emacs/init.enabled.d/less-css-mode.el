90604a362a319d449c643003e221ec69c8f0fefe
Desmond O. Chang <dochang@gmail.com>
2018-08-24 21:15:12 +0800

    Reconfigure `less-css-mode-hook`

--- emacs/init.enabled.d/less-css-mode.el
+++ emacs/init.enabled.d/less-css-mode.el
@@ -2,15 +2,7 @@
 ;; [[http://www.emacswiki.org/emacs/LessCssMode]]
 ;; [[https://github.com/purcell/less-css-mode]]
 
-(defun $less-css-mode-hook ()
-  ;; No need to run `prog-mode-hook' if `less-css-mode' is derived from
-  ;; `css-mode'.
-  (unless (derived-mode-p 'css-mode)
-    ($run-prog-mode-hook))
-  (rainbow-mode 1))
-
 (req-package less-css-mode
-  :hook (less-css-mode . $less-css-mode-hook)
   :init
   ;; Don't compile after saving.
   (setq less-css-compile-at-save nil))
