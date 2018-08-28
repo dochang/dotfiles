a99e9e1903226296afcaac03281afbd072f20fe5
Desmond O. Chang <dochang@gmail.com>
2018-08-29 00:36:25 +0800

    Do correct fix

--- emacs/init.enabled.d/flyspell.el
+++ emacs/init.enabled.d/flyspell.el
@@ -1,6 +1,18 @@
 ;; Flyspell
 
 (req-package flyspell
+  :demand t
+  ;; `org' will modify `flyspell-delayed-commands' after `flyspell' loaded.
+  ;; Unfortunately we have an init file called `flyspell.el' (this file).  It
+  ;; triggers the `eval-after-load' code, but at that moment
+  ;; `flyspell-delayed-commands' is undefined.  This will cause an error.
+  ;;
+  ;; To solve this issue, force to load `flyspell-delayed-commands' before
+  ;; loading `org'.
+  ;;
+  ;; Note: we can't declare `:require flyspell' in `(req-package org)'.
+  ;; Because req-package puts `:require flyspell' in the `:config' part of
+  ;; `(use-package org)', that means req-package loads flyspell after org.
   :bind (:map flyspell-mode-map
          ("M-t" . nil))
   :hook ((message-setup text-mode) . flyspell-mode)
