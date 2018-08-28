9a61edd5969730c456ab5309bcfbea4e3fd7d5a6
Desmond O. Chang <dochang@gmail.com>
2018-08-29 01:59:32 +0800

    Ensure hooks run

--- emacs/init.enabled.d/color-theme.el
+++ emacs/init.enabled.d/color-theme.el
@@ -1,5 +1,10 @@
 (req-package color-theme
   :config
+  (when after-init-time
+    (color-theme-backup-original-values))
+  ;; `color-theme' wants to run `color-theme-backup-original-values' in
+  ;; `after-init-hook'.  But we can't, since the library would not be loaded at
+  ;; that time.  We explicitly run this function after `color-theme' loaded.
   (color-theme-initialize)
   ;; NIL doesn't work any more.
   (setq color-theme-is-global t))
--- emacs/init.enabled.d/dashboard.el
+++ emacs/init.enabled.d/dashboard.el
@@ -2,4 +2,9 @@
   :init
   (setq dashboard-items '((agenda)))
   :config
+  (when after-init-time
+    (dashboard-insert-startupify-lists))
+  ;; `dashboard' wants to run `dashboard-insert-startupify-lists' in
+  ;; `after-init-hook'.  But we can't, since the library would not be loaded at
+  ;; that time.  We explicitly run this function after `dashboard' loaded.
   (dashboard-setup-startup-hook))
