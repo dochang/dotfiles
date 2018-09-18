90a2a25d01745f166e59142fe18e9a3ff8975265
Desmond O. Chang <dochang@gmail.com>
2018-09-19 03:34:03 +0800

    Add key bindings for smartparens

--- emacs/init.d/smartparens.el
+++ emacs/init.d/smartparens.el
@@ -1,5 +1,39 @@
 (req-package smartparens
+
   :hook ((emacs-startup . smartparens-global-strict-mode)
          (emacs-startup . show-smartparens-global-mode))
+
+  :custom
+
+  (sp-base-key-bindings nil)
+  ;; Do not use key bindings of neither paredit style nor smartparens style.
+  ;; They both bind too many keys.
+
+  (sp-override-key-bindings
+   '(
+
+     ("C-M-f" . sp-forward-sexp)
+     ("C-M-b" . sp-backward-sexp)
+     ("C-M-u" . sp-backward-up-sexp)
+     ("C-M-d" . sp-down-sexp)
+
+     ("C-M-k" . sp-kill-sexp)
+     ("C-M-SPC" . sp-mark-sexp)
+
+     ;; Do not bind these keys.  They have been bound to other commands.
+     ;;
+     ;; ("C-<right>" . sp-forward-slurp-sexp)
+     ;; ("C-<left>" . sp-forward-barf-sexp)
+     ;; ("C-M-<left>" . sp-backward-slurp-sexp)
+     ;; ("C-M-<right>" . sp-backward-barf-sexp)
+
+     ("C-)" . sp-forward-slurp-sexp)
+     ("C-}" . sp-forward-barf-sexp)
+     ("C-(" . sp-backward-slurp-sexp)
+     ("C-{" . sp-backward-barf-sexp)
+
+     ))
+
   :config
+
   (require 'smartparens-config))
