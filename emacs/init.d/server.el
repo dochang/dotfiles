1303adec0795c0320cc5415b944468a1c45e4908
Desmond O. Chang <dochang@gmail.com>
2018-09-28 17:33:12 +0800

    Fix `server-name` assignment

--- emacs/init.d/server.el
+++ emacs/init.d/server.el
@@ -1,7 +1,27 @@
 (req-package server
   :hook (emacs-startup . (lambda ()
-                           (unless (bound-and-true-p server-process)
+                           (unless (or (bound-and-true-p server-process)
+                                       (and (fboundp 'server-running-p)
+                                            (server-running-p server-name)))
                              (server-start))))
   :custom
-  (server-name (number-to-string (emacs-pid)))
+  (server-name (if (stringp (daemonp))
+                   (daemonp)
+                 (number-to-string (emacs-pid))))
+  ;; If Emacs is invoked with `--daemon=NAME', the name of server communication
+  ;; file will be NAME, which is returned by `daemonp'.  Otherwise, if Emacs is
+  ;; invoked with `--daemon' or no `--daemon', set `server-name' to Emacs pid.
+  ;;
+  ;; Actually Emacs changes `server-name' before `server-start' if
+  ;; `--daemon=NAME' is passed.  We do this explicitly for convenience of
+  ;; reloading configuration.
+  ;;
+  ;; When `--daemon[=NAME]' is passed, Emacs starts the server after
+  ;; `after-init-hook', before `emacs-startup-hook'.
+  ;;
+  ;; Order:
+  ;;
+  ;;   - Setting `server-name' in `load-dirs', which is in `after-init-hook'.
+  ;;   - `server-start'
+  ;;   - `emacs-startup-hook'
   (server-use-tcp t))
