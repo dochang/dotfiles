e21eb02840992d9b88400a600674e4577553c5d3
Desmond O. Chang <dochang@gmail.com>
2018-10-23 06:32:21 +0800

    Install focus

---
+++ emacs/init.d/focus.el
@@ -0,0 +1,16 @@
+(req-package focus
+  :hook (((prog-mode text-mode) . focus-mode)
+         ((org-mode) . (lambda () (focus-mode -1))))
+  :config
+  (make-variable-buffer-local 'focus-current-thing)
+  (make-variable-buffer-local 'focus-buffer)
+  (make-variable-buffer-local 'focus-pre-overlay)
+  (make-variable-buffer-local 'focus-post-overlay)
+  (make-variable-buffer-local 'focus-read-only-blink-timer)
+  ;; When the second time `focus-init' runs, the default values of
+  ;; `focus-pre-overlay' and `focus-post-overlay' will be set to their buffer
+  ;; local values.
+  ;;
+  ;; This is because of the misuse of `make-local-variable'.  Use
+  ;; `make-variable-buffer-local' instead.
+  )
