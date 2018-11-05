c63f88849c75852c7b5d8035afaf3d7a18171512
Desmond O. Chang <dochang@gmail.com>
2018-11-05 15:38:33 +0800

    Fix auto-package-update advice

--- emacs/init.d/auto-package-update.el
+++ emacs/init.d/auto-package-update.el
@@ -1,11 +1,35 @@
 ;;; auto-package-update
-(define-advice apu--package-out-of-date-p (:around (fn &rest r) unavailable-packages-return-nil)
-  "Non package.el packages (e.g. el-get packages) and Nonupgradeable packages
-  are NOT up-to-date and NOT out-of-date."
+
+;; Consider the following packages up-to-date.
+;;
+;; 1. A non package.el package
+;;
+;;    This means `(package-installed-p PKG)' is nil.
+;;
+;; 2. A package that `package-archive-contents' doesn't have
+;;
+;;    This means `(cadr (assq PKG package-archive-contents))' is nil.
+;;
+;; 3. A package that `(package-desc-status PKG-DESC)' is not:
+;;
+;;    - unsigned
+;;    - installed
+;;    - dependency
+
+(define-advice apu--package-up-to-date-p (:around (fn &rest r) pretest)
   (let ((pkg (car r)))
-    (and (package-installed-p pkg)
-         (cadr (assq pkg package-archive-contents))
-         (funcall fn pkg))))
+    (or (not (package-installed-p pkg))
+        (null (cadr (assq pkg package-archive-contents)))
+        (let* ((desc (or
+                      (if (package-desc-p pkg) pkg)
+                      (cadr (assq pkg package-alist))
+                      (let ((built-in (assq pkg package--builtins)))
+                        (if built-in
+                            (package--from-builtin built-in)
+                          (cadr (assq pkg package-archive-contents))))))
+               (status (package-desc-status desc)))
+          (not (member status '("unsigned" "installed" "dependency"))))
+        (apply fn r))))
 
 (req-package auto-package-update
   :hook (emacs-startup . auto-package-update-maybe)
