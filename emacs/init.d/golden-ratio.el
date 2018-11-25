561940391e73c3b6efb291595f9bd448b9a9f832
Desmond O. Chang <dochang@gmail.com>
2018-11-25 10:54:33 +0800

    Exclude calendar-mode from golden-ratio

--- emacs/init.d/golden-ratio.el
+++ emacs/init.d/golden-ratio.el
@@ -1,4 +1,5 @@
 (req-package golden-ratio
   :hook (emacs-startup . golden-ratio-mode)
   :custom
-  (golden-ratio-auto-scale t))
+  (golden-ratio-auto-scale t)
+  (golden-ratio-exclude-modes '(calendar-mode)))
