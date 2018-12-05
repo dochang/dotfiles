9ecf393411faef0184979790ad19dfb1fbb5c0da
Desmond O. Chang <dochang@gmail.com>
2018-12-06 04:20:28 +0800

    Do not require project root in projectile

--- emacs/init.d/projectile.el
+++ emacs/init.d/projectile.el
@@ -4,4 +4,9 @@
 
 (req-package projectile
   :hook ((emacs-startup . projectile-mode)
-         (projectile-after-switch-project . $projectile-after-switch-project-hook)))
+         (projectile-after-switch-project . $projectile-after-switch-project-hook))
+  :custom
+  (projectile-require-project-root nil)
+  ;; https://github.com/emacs-lsp/lsp-python/issues/28#issuecomment-437599058
+  ;; https://github.com/emacs-lsp/lsp-mode/pull/470#issuecomment-437600636
+  )
