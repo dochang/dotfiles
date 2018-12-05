cdf6c6dc4a70948ed7df21cf709d4c7a7ff80324
Desmond O. Chang <dochang@gmail.com>
2018-12-06 04:21:30 +0800

    Automatically guess root and fallback to cwd for lsp

--- emacs/init.d/lsp.el
+++ emacs/init.d/lsp.el
@@ -1,3 +1,11 @@
+(define-advice lsp--suggest-project-root (:after-until (&optional args) fallback-to-default-directory)
+  default-directory)
+
 (req-package lsp
   :ensure lsp-mode
-  :hook (prog-mode . lsp))
+  :hook (prog-mode . lsp)
+  :custom
+  (lsp-auto-guess-root t)
+  ;; https://github.com/emacs-lsp/lsp-python/issues/28#issuecomment-437599058
+  ;; https://github.com/emacs-lsp/lsp-mode/pull/470#issuecomment-437600636
+  )
