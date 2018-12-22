ee40c98a6ec810fc8b32f8aaef6464bbf2c1c613
Desmond O. Chang <dochang@gmail.com>
2018-12-22 09:03:29 +0800

    Explain why we still install auth-source-pass in Emacs 26

--- emacs/init.d/auth-source-pass.el
+++ emacs/init.d/auth-source-pass.el
@@ -1,2 +1,7 @@
+;; auth-source-pass has been included in Emacs 26.  But we want to use the
+;; latest version.
+;;
+;; https://github.com/DamienCassou/auth-password-store/blob/382faa2612f876f16bd53497d332148b98f840a8/README.md#installing
+
 (req-package auth-source-pass
   :hook (emacs-startup . auth-source-pass-enable))
