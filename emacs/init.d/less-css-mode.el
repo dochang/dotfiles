183510a59611eb03eaedf9376a37495a28320aa4
Desmond O. Chang <dochang@gmail.com>
2018-12-23 19:16:10 +0800

    Do not install less-css-mode on Emacs 26+

--- emacs/init.d/less-css-mode.el
+++ emacs/init.d/less-css-mode.el
@@ -2,7 +2,13 @@
 ;; [[http://www.emacswiki.org/emacs/LessCssMode]]
 ;; [[https://github.com/purcell/less-css-mode]]
 
+;; This package has been included in Emacs core at commit
+;; 3f887812e708123eca2f85cfbf5004e405aff914.
 (req-package less-css-mode
+  :ensure nil
   :custom
   ;; Don't compile after saving.
-  (less-css-compile-at-save nil))
+  (less-css-compile-at-save nil)
+  :init
+  (when (version< emacs-version "26")
+    (package-install 'less-css-mode)))
