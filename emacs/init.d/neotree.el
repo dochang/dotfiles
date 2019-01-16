03d93fd153776a9cff2fefdd36550736765a2b84
Desmond O. Chang <dochang@gmail.com>
2019-01-16 17:40:40 +0800

    Install neotree

---
+++ emacs/init.d/neotree.el
@@ -0,0 +1,13 @@
+(req-package neotree
+  :bind (("C-c t" . neotree-toggle))
+  :custom
+  (neo-show-updir-line t)
+  (neo-show-slash-for-folder t)
+  (neo-reset-size-on-open nil)
+  (neo-theme 'icons)
+  (neo-smart-open nil)
+  (neo-show-hidden-files nil)
+  (neo-autorefresh t)
+  (neo-window-fixed-size t)
+  (neo-toggle-window-keep-p nil)
+  (neo-hide-cursor t))
