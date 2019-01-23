3bc6a481f7ece83fa4d51e5fb5fb81e0fd9f30e9
Desmond O. Chang <dochang@gmail.com>
2019-01-23 17:12:46 +0800

    Install gruvbox-theme

---
+++ emacs/init.d/gruvbox-theme.el
@@ -0,0 +1,6 @@
+(req-package gruvbox-theme
+  :init
+  (add-to-list '**custom-themes** 'gruvbox)
+  (add-to-list '**custom-themes** 'gruvbox-dark-medium)
+  (add-to-list '**custom-themes** 'gruvbox-dark-soft)
+  (add-to-list '**custom-themes** 'gruvbox-dark-hard))
