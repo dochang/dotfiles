3d113a293990c92df492c65e0d3306f01a6d6902
Desmond O. Chang <dochang@gmail.com>
2019-01-23 21:49:19 +0800

    Install org-clock-budget

---
+++ emacs/init.d/org-clock-budget.el
@@ -0,0 +1,4 @@
+(req-package org-clock-budget
+  :ensure (org-clock-budget :pin :quelpa)
+  :quelpa (org-clock-budget :fetcher github :repo "Fuco1/org-clock-budget")
+  :commands (org-clock-budget-report))
