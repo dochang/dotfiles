ebbaff46cd1de256bc4bc3a5b9e8c3320c23073c
Desmond O. Chang <dochang@gmail.com>
2019-01-23 22:08:52 +0800

    Do not display-line-number-mode in org-mode
    See [1].
    
    [1]: https://github.com/cute-jumper/org-table-sticky-header/issues/10

--- emacs/init.d/display-line-numbers.el
+++ emacs/init.d/display-line-numbers.el
@@ -2,7 +2,7 @@
 (unless (version< emacs-version "26")
   (req-package display-line-numbers
     :ensure (display-line-numbers :pin :built-in)
-    :hook ((prog-mode adoc-mode conf-mode org-mode) . display-line-numbers-mode)
+    :hook ((prog-mode adoc-mode conf-mode) . display-line-numbers-mode)
     :custom
     (display-line-numbers-type t)
     (display-line-numbers-grow-only nil)
