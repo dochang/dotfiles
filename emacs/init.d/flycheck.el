c8d6abd5a6f545f3ee14bbbb069073a3150877cb
Desmond O. Chang <dochang@gmail.com>
2019-02-12 23:56:20 +0800

    Disable rubocop for flycheck
    Rubocop has many customizable options and is hard to configure.  Use rufo
    instead of it.

--- emacs/init.d/flycheck.el
+++ emacs/init.d/flycheck.el
@@ -1,5 +1,7 @@
 (req-package flycheck
   :hook (prog-mode . flycheck-mode)
+  :custom
+  (flycheck-disabled-checkers '(ruby-rubocop))
   :config
   (flycheck-yamllint-setup)
   (flycheck-package-setup)
