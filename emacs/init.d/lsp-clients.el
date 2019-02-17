d770145ca592617096811ed64853a2903c9fcbc9
Desmond O. Chang <dochang@gmail.com>
2019-02-18 05:06:24 +0800

    Revert "Replace go-langserver with bingo"
    This reverts commit 44b25a377ae7ec4f10e17502fd632c6a9d6a88ff.
    
    lsp-mode has switched [1] to bingo.
    
    [1]: https://github.com/emacs-lsp/lsp-mode/commit/245d5ad9bd6919c032b61193afcf86ce93ca0041

--- emacs/init.d/lsp-clients.el
+++ emacs/init.d/lsp-clients.el
@@ -1,9 +1,5 @@
 (req-package lsp-clients
   :ensure lsp-mode
   :after lsp
-  :custom
-  (lsp-clients-go-server "bingo")
-  (lsp-clients-go-executable-path (executable-find "bingo"))
-  (lsp-clients-go-language-server-flags '())
   :init
   (require 'lsp-clients))
