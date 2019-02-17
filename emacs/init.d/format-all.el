feb80f611b175ab4a61bb8bfcd4a230852c9cdb8
Desmond O. Chang <dochang@gmail.com>
2019-02-18 03:14:39 +0800

    Use prettier to format yaml files

--- emacs/init.d/format-all.el
+++ emacs/init.d/format-all.el
@@ -1,2 +1,14 @@
 (req-package format-all
-  :hook (prog-mode . format-all-mode))
+
+  :hook (prog-mode . format-all-mode)
+
+  :config
+
+  (puthash 'yaml-mode
+           `((prettier . ,(lambda () "yaml")))
+           format-all-mode-table)
+  ;; Use prettier to format yaml files instead of yq.
+  ;;
+  ;; https://github.com/mikefarah/yq/issues/25
+
+  )
