5cc7bfc07e40b5525a9d02d28946c97b8d698c64
Desmond O. Chang <dochang@gmail.com>
2019-02-19 11:21:01 +0800

    Add isort support in format-all

--- emacs/init.d/format-all.el
+++ emacs/init.d/format-all.el
@@ -11,4 +11,25 @@
   ;;
   ;; https://github.com/mikefarah/yq/issues/25
 
+  ;; Add isort for python-mode
+  ;;
+  ;; Note, currently format-all doesn't support multiple formatters for one
+  ;; buffer.
+  ;;
+  ;; https://github.com/lassik/emacs-format-all-the-code/issues/6
+  (puthash 'isort "isort" format-all-executable-table)
+  (puthash 'isort "pip install isort" format-all-install-table)
+  (puthash 'black
+           (lambda (executable mode-result)
+             (ignore mode-result)
+             ;; https://github.com/timothycrosley/isort/issues/40
+             (format-all-buffer-easy
+              "/bin/sh" "-c"
+              (format "%s %s %s | %s -q -"
+                      (format-all-formatter-executable 'isort)
+                      "--apply"
+                      "-"
+                      executable)))
+           format-all-format-table)
+
   )
