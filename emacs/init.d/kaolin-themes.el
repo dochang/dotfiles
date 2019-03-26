c5643135dee5c7dba8b20919e42f51449ae63d49
Desmond O. Chang <dochang@gmail.com>
2019-03-26 18:44:35 +0800

    Install kaolin-themes

---
+++ emacs/init.d/kaolin-themes.el
@@ -0,0 +1,11 @@
+(req-package kaolin-themes
+  :init
+  (add-to-list '**custom-themes** 'kaolin-aurora)
+  (add-to-list '**custom-themes** 'kaolin-bubblegum)
+  (add-to-list '**custom-themes** 'kaolin-dark)
+  (add-to-list '**custom-themes** 'kaolin-eclipse)
+  (add-to-list '**custom-themes** 'kaolin-galaxy)
+  (add-to-list '**custom-themes** 'kaolin-mono-dark)
+  (add-to-list '**custom-themes** 'kaolin-ocean)
+  (add-to-list '**custom-themes** 'kaolin-temple)
+  (add-to-list '**custom-themes** 'kaolin-valley-dark))
