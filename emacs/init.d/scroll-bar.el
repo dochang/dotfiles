89aa585ca956c4083419f6ef3e484a4c76c25e0d
Desmond O. Chang <dochang@gmail.com>
2019-04-22 16:49:15 +0800

    Call `set-scroll-bar-mode` only if it is defined

--- emacs/init.d/scroll-bar.el
+++ emacs/init.d/scroll-bar.el
@@ -1,3 +1,7 @@
 (req-package scroll-bar
   :ensure (scroll-bar :pin :built-in)
-  :hook (emacs-startup . (lambda () (set-scroll-bar-mode 'left))))
+  :hook (emacs-startup . (lambda ()
+                           (when (fboundp 'set-scroll-bar-mode)
+                             ;; If Emacs is built with `--without-x',
+                             ;; `set-scroll-bar-mode' will be undefined.
+                             (set-scroll-bar-mode 'left)))))
