9922ae9eac71f3e757f05b36ba11d52e158876dd
Desmond O. Chang <dochang@gmail.com>
2019-04-22 17:01:29 +0800

    Update source location of rdoc-mode
    https://github.com/ruby/ruby/commit/7eedd308b841e7b8eb4bc36211d28faf3521ee92
    
    https://github.com/ruby/elisp/blob/master/rdoc-mode.el

--- emacs/init.d/rdoc-mode.el
+++ emacs/init.d/rdoc-mode.el
@@ -1,6 +1,6 @@
 ;; There are 2 versions.
 ;;
-;;  1. [[https://github.com/ruby/ruby/blob/trunk/misc/rdoc-mode.el]]
+;;  1. https://github.com/ruby/elisp/blob/master/rdoc-mode.el
 ;;  2. [[https://github.com/jwiegley/ruby-mode/blob/master/rdoc-mode.el]]
 ;;
 ;; 1 is newer than 2.
@@ -8,8 +8,8 @@
 (req-package rdoc-mode
   :ensure (rdoc-mode :pin :el-get-bundle)
   :el-get-bundle (rdoc-mode
-                  :website "https://github.com/ruby/ruby/blob/trunk/misc/rdoc-mode.el"
+                  :website "https://github.com/ruby/elisp/blob/master/rdoc-mode.el"
                   :description "Major mode for RDoc editing"
                   :type http
-                  :url "https://raw.github.com/ruby/ruby/trunk/misc/rdoc-mode.el")
+                  :url "https://github.com/ruby/elisp/raw/master/rdoc-mode.el")
   :mode ("\\.rdoc\\'" . rdoc-mode))
