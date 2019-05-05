c41dd7187424134502146f5b7243e2e29d300747
Desmond O. Chang <dochang@gmail.com>
2019-05-05 06:46:18 +0000

    Indent 4 spaces in kotlin-mode

--- emacs/init.d/kotlin-mode.el
+++ emacs/init.d/kotlin-mode.el
@@ -6,4 +6,6 @@
   ;;
   ;; [[https://kotlinlang.org/docs/tutorials/command-line.html#using-the-command-line-to-run-scripts]]
   :mode ("\\.kts\\'" . kotlin-mode)
-  :hook (kotlin-mode . $kotlin-mode-hook))
+  :hook (kotlin-mode . $kotlin-mode-hook)
+  :custom
+  (kotlin-tab-width 4))
