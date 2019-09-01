780c1c13f9caed33b2241297de1119923b85ab5d
Desmond O. Chang <dochang@gmail.com>
2019-09-01 18:10:43 +0000

    Do not verify the insecure directories for zsh completions

--- shell/zprofile.zsh
+++ shell/zprofile.zsh
@@ -1,1 +1,7 @@
 {% include "profile.sh" %}
+
+# Do not verify the insecure directories for completions.
+#
+# https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/compfix.zsh
+: ${ZSH_DISABLE_COMPFIX:=true}
+export ZSH_DISABLE_COMPFIX
