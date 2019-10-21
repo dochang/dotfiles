961cb4f42034e592a5b74250f6e94159d1cde5f7
Desmond O. Chang <dochang@gmail.com>
2019-10-21 08:41:00 +0000

    Enable auto-dim-other-buffers-mode only on graphic display

--- emacs/init.d/auto-dim-other-buffers.el
+++ emacs/init.d/auto-dim-other-buffers.el
@@ -1,2 +1,4 @@
 (req-package auto-dim-other-buffers
-  :hook (emacs-startup . auto-dim-other-buffers-mode))
+  :hook ((emacs-startup . (lambda ()
+                            (auto-dim-other-buffers-mode
+                             (if (or (daemonp) (display-graphic-p)) 1 -1))))))
