e95ec48bd4ea2c56b14e00687fe37b088c9f5052
Desmond O. Chang <dochang@gmail.com>
2019-10-23 21:49:12 +0800

    Update format-all configuration

--- emacs/init.d/format-all.el
+++ emacs/init.d/format-all.el
@@ -4,9 +4,9 @@
 
   :config
 
-  (puthash 'yaml-mode
-           `((prettier . ,(lambda () "yaml")))
-           format-all-mode-table)
+  (format-all--pushhash 'yaml-mode
+                        `(prettier . ,(lambda () "yaml"))
+                        format-all--mode-table)
   ;; Use prettier to format yaml files instead of yq.
   ;;
   ;; https://github.com/mikefarah/yq/issues/25
@@ -17,39 +17,39 @@
   ;; buffer.
   ;;
   ;; https://github.com/lassik/emacs-format-all-the-code/issues/6
-  (puthash 'isort "isort" format-all-executable-table)
-  (puthash 'isort "pip install isort" format-all-install-table)
+  (puthash 'isort "isort" format-all--executable-table)
+  (puthash 'isort "pip install isort" format-all--install-table)
   (puthash 'black
            (lambda (executable mode-result)
              (ignore mode-result)
              ;; https://github.com/timothycrosley/isort/issues/40
-             (format-all-buffer-easy
+             (format-all--buffer-easy
               "/bin/sh" "-c"
               (format "%s %s %s | %s -q -"
-                      (format-all-formatter-executable 'isort)
+                      (format-all--formatter-executable 'isort)
                       "--apply"
                       "-"
                       executable)))
-           format-all-format-table)
+           format-all--format-table)
 
   ;; toml
   (puthash 'prettier
            "npm install --global prettier prettier-plugin-toml"
-           format-all-install-table)
-  (puthash 'toml-mode
-           `((prettier . ,(lambda () "toml")))
-           format-all-mode-table)
+           format-all--install-table)
+  (format-all--pushhash 'toml-mode
+                        `(prettier . ,(lambda () "toml"))
+                        format-all--mode-table)
 
   ;; jsonnet
-  (puthash 'jsonnet "jsonnet" format-all-executable-table)
-  (puthash 'jsonnet "brew install jsonnet" format-all-install-table)
-  (puthash 'jsonnet-mode
-           `((jsonnet . ,(lambda () "jsonnet")))
-           format-all-mode-table)
+  (puthash 'jsonnet "jsonnet" format-all--executable-table)
+  (puthash 'jsonnet "brew install jsonnet" format-all--install-table)
+  (format-all--pushhash 'jsonnet-mode
+                        `(jsonnet . ,(lambda () "jsonnet"))
+                        format-all--mode-table)
   (puthash 'jsonnet
            (lambda (executable mode-result)
              (ignore mode-result)
-             (format-all-buffer-easy executable "fmt" "-"))
-           format-all-format-table)
+             (format-all--buffer-easy executable "fmt" "-"))
+           format-all--format-table)
 
   )
