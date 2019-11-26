04683bc77e188df1fc2f41d7fc8d7c7340228ab9
Desmond O. Chang <dochang@gmail.com>
2019-11-26 15:16:06 +0800

    Disable snippet completion support

--- emacs/init.d/lsp-mode.el
+++ emacs/init.d/lsp-mode.el
@@ -9,4 +9,5 @@
   ;; https://github.com/emacs-lsp/lsp-python/issues/28#issuecomment-437599058
   ;; https://github.com/emacs-lsp/lsp-mode/pull/470#issuecomment-437600636
   (lsp-auto-configure t)
+  (lsp-enable-snippet nil)
   )
