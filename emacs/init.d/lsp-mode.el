d6c6eb7827cf9f6d28a2fb6a00f07dbba82f719b
Desmond O. Chang <dochang@gmail.com>
2019-12-08 16:58:21 +0800

    Install lsp functions into `before-save-hook`
    https://github.com/golang/tools/blob/master/gopls/doc/emacs.md#emacs

--- emacs/init.d/lsp-mode.el
+++ emacs/init.d/lsp-mode.el
@@ -1,9 +1,14 @@
 (define-advice lsp--suggest-project-root (:after-until (&optional args) fallback-to-default-directory)
   default-directory)
 
+(defun $lsp-install-before-save-hooks ()
+  (add-hook 'before-save-hook 'lsp-format-buffer nil 'local)
+  (add-hook 'before-save-hook 'lsp-organize-imports nil 'local))
+
 (req-package lsp-mode
   :ensure lsp-mode
-  :hook (prog-mode . lsp-deferred)
+  :hook ((prog-mode . lsp-deferred)
+         (prog-mode . $lsp-install-before-save-hooks))
   :custom
   (lsp-auto-guess-root t)
   ;; https://github.com/emacs-lsp/lsp-python/issues/28#issuecomment-437599058
