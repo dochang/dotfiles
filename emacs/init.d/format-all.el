f052b4245055703213d0d511ce274847cdc8c18c
Desmond O. Chang <dochang@gmail.com>
2020-04-03 00:51:21 +0800

    Migrate format-all configuration [1]
    Add isort-black formatter to format-all.
    
    [1]: https://github.com/lassik/emacs-format-all-the-code/commit/033d0f3c253c07d8f087a956d73e81133b78bb31

--- emacs/init.d/format-all.el
+++ emacs/init.d/format-all.el
@@ -10,19 +10,16 @@
   ;; buffer.
   ;;
   ;; https://github.com/lassik/emacs-format-all-the-code/issues/6
-  (puthash 'isort "isort" format-all--executable-table)
-  (puthash 'isort "pip install isort" format-all--install-table)
-  (puthash 'black
-           (lambda (executable mode-result)
-             (ignore mode-result)
-             ;; https://github.com/timothycrosley/isort/issues/40
-             (format-all--buffer-easy
-              "/bin/sh" "-c"
-              (format "%s %s %s | %s -q -"
-                      (format-all--formatter-executable 'isort)
-                      "--apply"
-                      "-"
-                      executable)))
-           format-all--format-table)
+  ;;
+  ;; No need to remove black from `format-all--language-table'.  The last
+  ;; defined will be the first.
+  (define-format-all-formatter isort-black
+    (:executable "/bin/sh")
+    (:install "pip install isort black")
+    (:languages "Python")
+    (:format (format-all--buffer-easy
+              executable "-c"
+              (format "isort --apply - | black -q %s -"
+                      (if (format-all--buffer-extension-p "pyi") "--pyi" "")))))
 
   )
