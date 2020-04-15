6a0aa6c78ff2a8af147fa48ed7e3e71d7d33b1cb
Desmond O. Chang <dochang@gmail.com>
2020-04-16 02:50:56 +0800

    Do not upgrade quelpa packages at startup

--- emacs/init.d/quelpa.el
+++ emacs/init.d/quelpa.el
@@ -5,17 +5,31 @@
   :demand t
   ;; ensure: Ensure the package installed when the form loaded.
   :ensure t
+
+  ;; :hook (emacs-startup . quelpa-upgrade-all-maybe)
+  ;;
+  ;; Don't run `quelpa-upgrade-all-maybe' at startup.  I don't want to wait for
+  ;; it at startup.
+
   :custom
+
+  (quelpa-upgrade-interval 7)
+  ;; Be consistent with `auto-package-update-interval' but don't use it
+  ;; directly since it may not be set at this moment.
+
   (quelpa-upgrade-p nil)
   ;; If `quelpa-upgrade-p' is `t', Emacs tries to upgrade quelpa packages when
   ;; the `use-package' macro is evaluated.  This causes Emacs connects to
   ;; remote sites every time it starts.  We can't wait for it.  Upgrade quelpa
   ;; packages manually please.
+
   (quelpa-self-upgrade-p t)
   ;; Upgrade quelpa itself when doing a `quelpa-upgrade-all'.
+
   (quelpa-update-melpa-p nil)
   ;; Do not update MELPA git repo every time quelpa initialized.  Only update
   ;; the repo on `quelpa-upgrade' or `quelpa-self-upgrade'.
+
   )
 
 (define-advice quelpa-build--build-single-file-package (:around (fn &rest r) dont-update-time-stamp-and-copyright)
