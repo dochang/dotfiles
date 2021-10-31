d5ebc5ddf2cacf740cc71b8c1bb60bea431d98b3
Wade Zhang <dochang@gmail.com>
2021-11-01 02:42:27 +0800

    Format Go files using goimports for format-all
    format-all now supports goimports

--- playbooks/emacs/init.d/go-mode.el
+++ playbooks/emacs/init.d/go-mode.el
@@ -3,21 +3,10 @@
 
 (defun $go-mode-hook ()
   ($camel-case-mode 1)
-  ;; (add-hook 'before-save-hook '$gofmt-before-save 'append 'local)
+  ;; (add-hook 'before-save-hook 'gofmt-before-save 'append 'local)
+  (setq format-all-formatters '(("Go" goimports)))
   ;; Format source code by format-all.
   (set (make-local-variable 'company-backends) '(company-go)))
-
-(defun $gofmt-before-save ()
-  (interactive)
-  (mapc (lambda (cmdline)
-          (let ((gofmt-command (car cmdline))
-                (gofmt-args (cdr cmdline)))
-            (gofmt-before-save)))
-        (cl-remove-if-not (lambda (cmdline)
-                            (let ((gofmt-command (car cmdline)))
-                              (executable-find gofmt-command)))
-                          '(("goimports")
-                            ("gofmt" "-s")))))
 
 (req-package go-mode
   :bind (:map go-mode-map
@@ -25,6 +14,9 @@
          ("M-*" . pop-tag-mark))
   :hook (go-mode . $go-mode-hook)
   :init
-  (setq gofmt-command
-        (cond ((executable-find "goimports") "goimports")
-              (t "gofmt"))))
+  (cond ((executable-find "goimports")
+         (setq gofmt-command "goimports"
+               gofmt-args nil))
+        (t
+         (setq gofmt-command "gofmt"
+               gofmt-args '("-s")))))
