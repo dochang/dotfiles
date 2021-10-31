ddf90040495ba7d61ec68a2da4e1025489f4f342
Wade Zhang <dochang@gmail.com>
2021-11-01 03:12:14 +0800

    Redefine isort formatter for format-all
    format-all now supports multiple formatters [1], [2], [3].
    
    [1]: https://github.com/lassik/emacs-format-all-the-code/commit/beefe5e405f69df6105c0ce708ceb63d8bd0b5b8
    [2]: https://github.com/lassik/emacs-format-all-the-code/issues/6#issuecomment-588324995
    [3]: https://github.com/lassik/emacs-format-all-the-code/issues/27#issuecomment-731600683

--- playbooks/emacs/init.d/format-all.el
+++ playbooks/emacs/init.d/format-all.el
@@ -5,22 +5,13 @@
 
   :config
 
-  ;; Add isort for python-mode
-  ;;
-  ;; Note, currently format-all doesn't support multiple formatters for one
-  ;; buffer.
-  ;;
-  ;; https://github.com/lassik/emacs-format-all-the-code/issues/6
-  ;;
-  ;; No need to remove black from `format-all--language-table'.  The last
-  ;; defined will be the first.
-  (define-format-all-formatter isort-black
-    (:executable "/bin/sh")
-    (:install "pip install isort black")
+  ;; Support isort for Python.
+  (define-format-all-formatter isort
+    (:executable "isort")
+    (:install "pip install isort[requirements,pipfile,pyproject]")
     (:languages "Python")
+    (:features)
     (:format (format-all--buffer-easy
-              executable "-c"
-              (format "isort --apply - | black -q %s -"
-                      (if (format-all--buffer-extension-p "pyi") "--pyi" "")))))
+              executable "-q" "-")))
 
   )
--- playbooks/emacs/init.d/python.el
+++ playbooks/emacs/init.d/python.el
@@ -1,1 +1,5 @@
-(req-package python)
+(defun $python-mode-hook ()
+  (setq format-all-formatters '(("Python" isort black))))
+
+(req-package python
+  :hook (python-mode . $python-mode-hook))
