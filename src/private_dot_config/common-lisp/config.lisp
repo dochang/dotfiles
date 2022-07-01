13e8a912bb238a9923b946e41f69883bf6f86c22
Wade Zhang <dochang@gmail.com>
2022-07-01 17:35:27 +0800

    wip

---
+++ src/private_dot_config/common-lisp/config.lisp
@@ -0,0 +1,10 @@
+(cl:in-package :cl-user)
+
+;; hot-upgrade ASDF
+(asdf:load-system :asdf)
+
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
