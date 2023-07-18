acf808c80f74dd3509c161f57b4f4b7dc90bf59a
Wade Zhang <dochang@gmail.com>
2023-07-18 16:37:23 +0800

    Specify font for wezterm

--- src/dot_config/wezterm/wezterm.lua
+++ src/dot_config/wezterm/wezterm.lua
@@ -3,5 +3,22 @@ local config = {}
 
 config.use_ime = true
 config.xim_im_name = "fcitx"
+config.font = wezterm.font_with_fallback({
+  {
+    family = "Sarasa Fixed SC",
+  },
+  {
+    family = "Noto Sans Mono CJK SC",
+  },
+  {
+    family = "HanaMinB",
+  },
+  {
+    family = "Noto Sans Mono",
+  },
+  {
+    family = "DejaVu Sans Mono",
+  },
+})
 
 return config
