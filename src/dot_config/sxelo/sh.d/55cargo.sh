1a2544a13a21ac11926f3c4cde219aa32a007ac8
Wade Zhang <dochang@gmail.com>
2023-09-27 12:29:00 +0800

    Fix Cargo

--- src/dot_config/sxelo/sh.d/55cargo.sh
+++ src/dot_config/sxelo/sh.d/55cargo.sh
@@ -1,6 +1,8 @@
 # shellcheck source=/dev/null
-[ ! -r "${CARGO_HOME}/env" ] || . "${CARGO_HOME}/env"
-if [ -n "${CARGO_INSTALL_ROOT}" ]; then
+
+if [ -r "${CARGO_HOME}/env" ]; then
+	. "${CARGO_HOME}/env"
+elif [ -n "${CARGO_INSTALL_ROOT}" ]; then
 	PATH="${CARGO_INSTALL_ROOT}/bin${PATH:+:}$PATH"
 else
 	PATH="${CARGO_HOME}/bin${PATH:+:}$PATH"
