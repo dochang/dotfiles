779ba8ad547d9f40986a6ccfd964138914c9739b
Wade Zhang <dochang@gmail.com>
2023-11-05 10:53:11 +0800

    Do not enable `lisp-local` in `lisp-data-mode`

--- src/dot_config/emacs/init.1.d/lisp-local.el
+++ src/dot_config/emacs/init.1.d/lisp-local.el
@@ -1,6 +1,11 @@
 (setup (:package lisp-local)
 
-  (:with-mode (lisp-data-mode scheme-mode clojure-mode)
+  (:with-mode (emacs-lisp-mode lisp-mode scheme-mode clojure-mode)
     (:hook lisp-local))
+  ;; `lisp-local' does not support `lisp-data-mode'.
+  ;;
+  ;; See `lisp-local--indent-properties'.
+  ;;
+  ;; https://github.com/lispunion/emacs-lisp-local/blob/22e221c9330d2b5dc07e8b2caa34c83ac7c20b0d/lisp-local.el#L120-L129
 
   )
