7a2fafa66c887dc97ce21c6fd22268baedfd3786
Wade Zhang <dochang@gmail.com>
2023-11-06 12:26:24 +0800

    tree-sitter

---
+++ src/dot_config/emacs/init.1.d/tree-sitter.el
@@ -0,0 +1,9 @@
+(when (< emacs-major-version 29)
+  ;; Use the built-in integration instead of this package on Emacs 29+.
+  ;;
+  ;; https://github.com/emacs-tree-sitter/elisp-tree-sitter/commit/d3eab879e9b0ccc9582113d81a93ad06f3c6bfb1
+  ;; https://emacs-tree-sitter.github.io/
+
+  (setup (:package tree-sitter))
+
+  )
