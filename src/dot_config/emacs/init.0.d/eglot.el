fe53547e6e665722d109b50d52dc7b6f34fc80c3
Wade Zhang <dochang@gmail.com>
2024-01-14 03:23:44 +0800

    Fix eglot error for json modes

--- src/dot_config/emacs/init.0.d/eglot.el
+++ src/dot_config/emacs/init.0.d/eglot.el
@@ -7,6 +7,23 @@
 (define-advice eglot-ensure (:before-while () lookup-before-ensure)
   (and buffer-file-name (eglot--lookup-mode major-mode)))
 
+(define-advice eglot--languageId (:override (&optional server &rest args) fix-json-modes)
+  "Because these json modes are derived from `js-mode', so return
+nil if `major-mode' is a json mode and `mode' is a js mode."
+  (setq server (or server (eglot--current-server-or-lose)))
+  (cl-loop for (mode . languageid) in
+           (eglot--languages server)
+           when (cond ((and (provided-mode-derived-p major-mode 'js-json-mode 'json-mode)
+                            (provided-mode-derived-p mode 'js-mode 'javascript-mode)
+                            (not (provided-mode-derived-p mode 'js-json-mode 'json-mode)))
+                       nil)
+                      ;; Because these json modes are derived from `js-mode',
+                      ;; so return nil if `major-mode' is a json mode and
+                      ;; `mode' is a js mode.
+                      (t
+                       (provided-mode-derived-p major-mode mode)))
+           return languageid))
+
 (setup (:package eglot)
 
   (:with-mode (prog-mode conf-mode text-mode)
