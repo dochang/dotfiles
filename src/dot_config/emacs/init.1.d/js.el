c5f6599f4285b97560bafe729e488cac52143ab4
Wade Zhang <dochang@gmail.com>
2024-01-29 19:35:11 +0800

    Enable `$camel-case-mode` in `js-base-mode`

--- src/dot_config/emacs/init.1.d/js.el
+++ src/dot_config/emacs/init.1.d/js.el
@@ -1,7 +1,12 @@
+(defun $js-base-mode-hook ()
+  ($camel-case-mode 1))
+
 (setup js
 
   (setq auto-mode-alist
         (append '(("\\.[cm]js\\'" . js-mode))
                 auto-mode-alist))
+
+  (add-hook 'js-base-mode-hook '$js-base-mode-hook)
 
   )
