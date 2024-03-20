4f9dff466d68c364bfd57c211c29bfbefcb253a3
Wade Zhang <dochang@gmail.com>
2024-03-20 20:51:10 +0800

    Configure Android

---
+++ src/dot_config/sxelo/sh.d/55android.sh
@@ -0,0 +1,4 @@
+# https://developer.android.com/tools/variables#set
+export ANDROID_HOME="${HOME}/opt/android"
+PATH="$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
+PATH="$ANDROID_HOME/cmdline-tools/latest/bin:$PATH"
