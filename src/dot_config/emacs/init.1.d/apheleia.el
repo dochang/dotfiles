7f9436f896e2f96421580c135f6f829e3cf1ba35
Wade Zhang <dochang@gmail.com>
2024-03-28 12:58:51 +0800

    Configure apheleia

--- src/dot_config/emacs/init.1.d/apheleia.el
+++ src/dot_config/emacs/init.1.d/apheleia.el
@@ -3,4 +3,24 @@
   ;; (add-hook 'emacs-startup-hook 'apheleia-global-mode)
   ;; Prefer format-all.
 
+  (:when-loaded
+
+    (setf (alist-get 'zigfmt apheleia-formatters)
+          '("zig" "fmt" "--stdin")
+          (alist-get 'taplofmt apheleia-formatters)
+          '("taplo" "fmt" "-"))
+
+    (setf (alist-get 'zig-mode apheleia-mode-alist)
+          'zigfmt
+          (alist-get 'toml-ts-mode apheleia-mode-alist)
+          'taplofmt
+          (alist-get 'toml-mode apheleia-mode-alist)
+          'taplofmt
+          (alist-get 'python-ts-mode apheleia-mode-alist)
+          '(black isort)
+          (alist-get 'python-mode apheleia-mode-alist)
+          '(black isort))
+
+    )
+
   )
