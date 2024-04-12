ea22d9d054a0eaa09ee896e35eb53a9c2df2058d
Wade Zhang <dochang@gmail.com>
2024-04-12 12:39:30 +0800

    Track org clock by Orgzly instead of Emacs

--- src/dot_config/emacs/init.1.d/org-clock.el
+++ src/dot_config/emacs/init.1.d/org-clock.el
@@ -5,13 +5,6 @@
     ;; Also insert clocking info into the drawer "LOGBOOK".
     (:option org-clock-into-drawer t)
 
-    ;; Resolve open clocks if we're idle more than 5 mins.
-    (:option org-clock-idle-time 5)
-
-    ;; Save the running clock when Emacs is closed.  The clock is
-    ;; resumed when Emacs restarts.
-    (:option org-clock-persist 'clock)
-
     )
 
   )
