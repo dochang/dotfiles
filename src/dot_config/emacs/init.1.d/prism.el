af91be07d1c1c985ba57a62c2cb348aab444fe84
Wade Zhang <dochang@gmail.com>
2024-04-23 02:24:49 +0800

    Enable `prism-mode` only in a graphic display

--- src/dot_config/emacs/init.1.d/prism.el
+++ src/dot_config/emacs/init.1.d/prism.el
@@ -4,16 +4,20 @@
   ;;
   ;; Ensure at least one theme is loaded (by `$set-theme') before enabling
   ;; `prism-mode'.
+  ;;
+  ;; Also ensure Emacs is running in a graphic display.
   (progn
 
     (:with-mode (prog-mode)
       (:hook (lambda ()
-               (when custom-enabled-themes
+               (when (and (display-graphic-p)
+                          custom-enabled-themes)
                  (prism-mode 1)))))
 
     (:with-mode (python-base-mode sh-base-mode)
       (:hook (lambda ()
-               (when custom-enabled-themes
+               (when (and (display-graphic-p)
+                          custom-enabled-themes)
                  (prism-mode -1)
                  (prism-whitespace-mode 1)))))
 
