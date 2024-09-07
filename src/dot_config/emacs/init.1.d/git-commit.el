68a6c2925e593825892fddf9bcf232cae63832fc
Wade Zhang <dochang@gmail.com>
2024-09-07 16:11:16 +0800

    Migrate `git-commit`

--- src/dot_config/emacs/init.1.d/git-commit.el
+++ src/dot_config/emacs/init.1.d/git-commit.el
@@ -1,4 +1,9 @@
-(setup (:package git-commit)
+(setup git-commit
+  (:package magit)
+  ;; `git-commit' has been merged into `magit'.  It must not be installed, in
+  ;; order to load the proper `git-commit' library from `magit'.
+  ;;
+  ;; https://github.com/magit/magit/commit/9be8a4ab7a7dc6f34615f1011e8da263651c8f87
 
   (:when-loaded
 
