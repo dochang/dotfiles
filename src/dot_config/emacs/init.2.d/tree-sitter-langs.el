d61b2f02b1c2577f463ae6b5f90bdb60c6ca7c7f
Wade Zhang <dochang@gmail.com>
2024-11-09 21:03:32 +0800

    Use tree-sitter-langs from MELPA
    It continues updating.

--- src/dot_config/emacs/init.2.d/tree-sitter-langs.el
+++ src/dot_config/emacs/init.2.d/tree-sitter-langs.el
@@ -1,8 +1,1 @@
-(when (< emacs-major-version 29)
-  ;; Use the built-in integration instead of this package on Emacs 29+.
-  ;;
-  ;; https://github.com/emacs-tree-sitter/elisp-tree-sitter/commit/d3eab879e9b0ccc9582113d81a93ad06f3c6bfb1
-
-  (setup (:package tree-sitter-langs))
-
-  )
+(setup (:package tree-sitter-langs))
