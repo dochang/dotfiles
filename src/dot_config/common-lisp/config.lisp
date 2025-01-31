5eea00139d70429b52ffbaa8cdf09ba2a1f96e8d
Wade Zhang <dochang@gmail.com>
2025-02-01 02:33:30 +0800

    Update Common Lisp config

--- src/dot_config/common-lisp/config.lisp
+++ src/dot_config/common-lisp/config.lisp
@@ -3,8 +3,10 @@
 ;; hot-upgrade ASDF
 (asdf:load-system :asdf)
 
+;;; The following lines added by ql:add-to-init-file:
 #-quicklisp
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
                                        (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
+
