d3ae02310496a02d7a9d5e3c3331af7189ccb3d1
Wade Zhang <dochang@gmail.com>
2025-03-22 22:33:58 +0800

    Install eat

---
+++ src/dot_config/emacs/init.2.d/eat.el
@@ -0,0 +1,21 @@
+(setup (:package eat)
+
+  (setq eat-line-input-history-isearch 'dwim)
+
+  (setq eat-eshell-fallback-if-stty-not-available t)
+
+  (setq eat-enable-mouse t)
+
+  (setq eat-enable-kill-from-terminal t)
+
+  (setq eat-enable-yank-to-terminal t)
+
+  (setq eat-enable-directory-tracking t)
+
+  (setq eat-enable-shell-prompt-annotation t)
+
+  (setq eat-enable-auto-line-mode nil)
+
+  (setq eat-query-before-killing-running-terminal 'auto)
+
+  )
