7e03e40b2f9034779994ae31de629c07046ffbe9
Wade Zhang <dochang@gmail.com>
2025-03-23 16:52:00 +0800

    Bind more convenient keys for `indent-rigidly`

--- src/dot_config/emacs/init.2.d/indent.el
+++ src/dot_config/emacs/init.2.d/indent.el
@@ -2,4 +2,10 @@
 
   (setq tab-always-indent 'complete)
 
+  (keymap-set indent-rigidly-map "<backtab>" 'indent-rigidly-left)
+  (keymap-set indent-rigidly-map "<" 'indent-rigidly-left)
+  (keymap-set indent-rigidly-map ">" 'indent-rigidly-right)
+  (keymap-set indent-rigidly-map "M-<" 'indent-rigidly-left-to-tab-stop)
+  (keymap-set indent-rigidly-map "M->" 'indent-rigidly-right-to-tab-stop)
+
   )
