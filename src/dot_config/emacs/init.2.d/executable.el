7fbb165972bd4a5d9414342dceca51627b3a838d
Wade Zhang <dochang@gmail.com>
2025-03-27 02:42:49 +0800

    Make script file executable after saving

---
+++ src/dot_config/emacs/init.2.d/executable.el
@@ -0,0 +1,9 @@
+(defun $executable-make-buffer-file-executable-if-script-p ()
+  (when (derived-mode-p 'prog-mode)
+    (executable-make-buffer-file-executable-if-script-p)))
+
+(setup executable
+
+  (add-hook 'after-save-hook '$executable-make-buffer-file-executable-if-script-p)
+
+  )
