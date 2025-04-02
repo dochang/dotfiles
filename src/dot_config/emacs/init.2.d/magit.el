e738f0a944859f81a179c1c62c8c02242f8e993e
Wade Zhang <dochang@gmail.com>
2025-04-02 23:31:32 +0800

    Enable all magit key binding sets manually

--- src/dot_config/emacs/init.2.d/magit.el
+++ src/dot_config/emacs/init.2.d/magit.el
@@ -5,7 +5,19 @@
 
 (setup (:package magit)
 
-  ;; Enable global key bindings
-  (setq magit-define-global-key-bindings 'recommended)
+  (setq magit-define-global-key-bindings nil)
+  ;; Do not enable key bindings automatically.
+
+  (add-hook 'emacs-startup-hook
+            (lambda ()
+              (mapc (lambda (set)
+                      (let ((magit-define-global-key-bindings set))
+                        (magit-maybe-define-global-key-bindings 'force)))
+                    '(recommended default))))
+  ;; Enable all key binding sets manually.
+  ;;
+  ;; `magit-maybe-define-global-key-bindings' may be run in `after-init-hook'
+  ;; and before this config.  `magit-define-global-key-bindings' can not be
+  ;; set in `eval-after-load'.
 
   )
