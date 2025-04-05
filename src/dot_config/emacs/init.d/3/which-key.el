dcad179d7c33a963709abb65eece572325cc6471
Wade Zhang <dochang@gmail.com>
2025-04-06 03:28:11 +0800

    Do not use C-h for which-key

--- src/dot_config/emacs/init.d/3/which-key.el
+++ src/dot_config/emacs/init.d/3/which-key.el
@@ -6,6 +6,8 @@
 
     (which-key-setup-side-window-right-bottom)
 
+    (setopt which-key-use-C-h-commands nil)
+
     (setopt which-key-side-window-max-width 0.333)
 
     (setopt which-key-sort-order 'which-key-key-order-alpha)
