85166ea95b410e5c05026905e5cae39617b5a2a2
Wade Zhang <dochang@gmail.com>
2025-07-13 10:28:26 +0800

    Ignore unsupported LSP features

--- src/dot_config/emacs/init.d/2/eglot.el
+++ src/dot_config/emacs/init.d/2/eglot.el
@@ -2,13 +2,13 @@
   (require 'eglot)
   ;; `eglot-managed-p' is not autoloaded.  Require `eglot' first.
   (when (eglot-managed-p)
-    (eglot-code-action-organize-imports (point-min) (point-max))
+    (ignore-errors (eglot-code-action-organize-imports (point-min) (point-max)))
     ;; https://github.com/golang/tools/blob/master/gopls/doc/emacs.md#organizing-imports-with-eglot
     (unless (derived-mode-p '(js-base-mode typescript-ts-base-mode))
       ;; https://www.reddit.com/r/emacs/comments/zam7ja/comment/krww5gs/
       ;; https://github.com/joaotavora/eglot/issues/157
       ;; https://github.com/typescript-language-server/typescript-language-server/issues/202#issuecomment-873469507
-      (eglot-format-buffer))))
+      (ignore-errors (eglot-format-buffer)))))
 
 ;; Run `eglot-ensure' only if the mode of `major-mode' has a language server.
 (defun $eglot-ensure ()
