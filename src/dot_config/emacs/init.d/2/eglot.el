96bae90e41ac6310a6bc936ed26a2201af3b3b73
Wade Zhang <dochang@gmail.com>
2025-10-16 10:31:15 +0800

    Exclude modes for eglot-format

--- src/dot_config/emacs/init.d/2/eglot.el
+++ src/dot_config/emacs/init.d/2/eglot.el
@@ -1,10 +1,16 @@
+(defvar **eglot-format-exclude-mode-list**
+  '(js-base-mode
+    typescript-ts-base-mode
+    yaml-ts-mode
+    yaml-mode))
+
 (defun $eglot-before-save-hook ()
   (require 'eglot)
   ;; `eglot-managed-p' is not autoloaded.  Require `eglot' first.
   (when (eglot-managed-p)
     (ignore-errors (eglot-code-action-organize-imports (point-min) (point-max)))
     ;; https://github.com/golang/tools/blob/master/gopls/doc/emacs.md#organizing-imports-with-eglot
-    (unless (derived-mode-p '(js-base-mode typescript-ts-base-mode))
+    (unless (derived-mode-p **eglot-format-exclude-mode-list**)
       ;; https://www.reddit.com/r/emacs/comments/zam7ja/comment/krww5gs/
       ;; https://github.com/joaotavora/eglot/issues/157
       ;; https://github.com/typescript-language-server/typescript-language-server/issues/202#issuecomment-873469507
