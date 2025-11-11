0e14538aa25ec3ca2c9c21cf4608f78d4316b802
Wade Zhang <dochang@gmail.com>
2025-11-11 16:15:43 +0800

    Install pyim

---
+++ src/dot_config/emacs/init.d/3/pyim.el
@@ -0,0 +1,21 @@
+(setup (:package pyim)
+
+  (with-eval-after-load 'pyim
+
+    (pyim-default-scheme 'quanpin)
+
+    )
+
+  (with-eval-after-load 'pyim-cloudim
+
+    (setopt pyim-cloudim nil)
+
+    )
+
+  (with-eval-after-load 'pyim-page
+
+    (setopt pyim-page-length 10)
+
+    )
+
+  )
