be3c816ec023fc53ab8609acaccda8e5fc814081
Wade Zhang <dochang@gmail.com>
2025-11-19 00:14:11 +0800

    Customize key bindings of pyim

--- src/dot_config/emacs/init.d/3/pyim.el
+++ src/dot_config/emacs/init.d/3/pyim.el
@@ -4,6 +4,21 @@
 
     (pyim-default-scheme 'quanpin)
 
+    (mapc (lambda (key-bindings)
+            (apply #'keymap-set pyim-mode-map key-bindings))
+          '(("C-@" pyim-select-word-simple)
+            ("M-d" pyim-delete-forward-imelem)
+            ("C-n" pyim-next-page)
+            ("C-p" pyim-previous-page)
+            ("C-f" pyim-next-word)
+            ("C-b" pyim-previous-word)
+            ("<down>" pyim-next-page)
+            ("<up>" pyim-previous-page)
+            ("<right>" pyim-next-word)
+            ("<left>" pyim-previous-word)
+            ("M-n" nil)
+            ("M-p" nil)))
+
     )
 
   (with-eval-after-load 'pyim-cloudim
