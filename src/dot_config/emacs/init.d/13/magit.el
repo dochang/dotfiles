43f39226282ddf2463a9d78f05577ecf888c7648
Wade Zhang <dochang@gmail.com>
2026-01-19 21:56:56 +0800

    Merge magit config

--- src/dot_config/emacs/init.d/13/magit.el
+++ src/dot_config/emacs/init.d/13/magit.el
@@ -20,4 +20,29 @@
   ;; and before this config.  `magit-define-global-key-bindings' can not be
   ;; set in `eval-after-load'.
 
+  (with-eval-after-load 'git-commit
+    ;; `git-commit' has been merged into `magit'.  It must not be installed, in
+    ;; order to load the proper `git-commit' library from `magit'.
+    ;;
+    ;; https://github.com/magit/magit/commit/9be8a4ab7a7dc6f34615f1011e8da263651c8f87
+
+    (keymap-set git-commit-redundant-bindings "C-M-i" 'completion-at-point)
+    ;; For conventional-commit
+
+    )
+
+  (with-eval-after-load 'magit-mode
+
+    ;; Don't pop a new window.
+    (setopt magit-display-buffer-function
+            'magit-display-buffer-same-window-except-diff-v1)
+
+    )
+
+  (with-eval-after-load 'magit-status
+
+    (setopt magit-status-show-hashes-in-headers t)
+
+    )
+
   )
